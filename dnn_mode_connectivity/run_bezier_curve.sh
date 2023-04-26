916f56cad53d86ee079d9ada776f579c32ce6702
Elluito <alfredoavendano@hotmail.com>
2023-04-26 19:14:48 +0100

    VGG19 on CIFAR10 GF data

---
+++ dnn_mode_connectivity/run_bezier_curve.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+# Submission script for serial Python job
+# Luis Alfredo Avendano  2021-10-22
+
+# Run from the current directory and with current environment
+#$ -cwd -V
+
+# Ask for some time (hh:mm:ss max of 48:00:00)
+#$ -l h_rt=012:00:00
+
+# ASk for some GPU
+#$ -l coproc_p100=1
+
+# Ask for some memory (by default, 1G, without a request)
+#$ -l h_vmem=12G
+# -pe smp 3
+# Send emails when job starts and ends
+#$ -m e
+#export OMP_NUM_THREADS=3
+#export MKL_NUM_THREADS=3
+#export OPENBLAS_NUM_THREADS=1
+# Now run the job
+#module load intel openmpi
+module add anaconda
+module add cuda/11.1.1
+conda activate work
+which python
+#nvcc --version
+#~python $1 $2 $3 $4 $5 $6
+python train.py --dir=curves --dataset=CIFAR10 --use_test --transform=ResNet --data_path=/nobackup/sclaam/data/CIFAR10 --model=ResNet18 --curve=Bezier --num_bends=3 --init_start=$1  --init_end=$2 --epochs=200 --lr=0.03 --wd=3e-4 --fix_end --fix_start
