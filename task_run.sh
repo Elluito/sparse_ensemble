b1ed6c25de4e46643d8ac5455ac1b2d7127f7553
Elluito <alfredoavendano@hotmail.com>
2023-05-17 16:34:57 +0100

    resnet50 cifar100 curve experiments

--- task_run.sh
+++ task_run.sh
@@ -31,7 +31,7 @@ conda activate work
 which python
 #nvcc --version
 #~python $1 $2 $3 $4 $5 $6
-python main.py -exp 6 -bs 128 --sigma $1 --pruner $2 --architecture $3 --dataset $4 --pruning_rate $5
+python main.py -exp 11 -bs 128 --sigma $1 --pruner $2 --architecture $3 --dataset $4 --pruning_rate $5
 
 
 #&& python main.py && python main.py
