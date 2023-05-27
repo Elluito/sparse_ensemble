91e8eddb702d6dc1b97a06729eaf402c92389cda
Elluito <alfredoavendano@hotmail.com>
2023-05-27 14:23:26 +0100

    script to send the jobs I need.

---
+++ run_pruning_rate_experiments.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l
+#Noise level
+# shellcheck disable=SC2054
+noise=("0.001" "0.003" "0.005")
+
+#Pruning level
+pruning=("0.8" "0.85" "0.9" "0.95")
+#Models
+models=("resnet18" "resnet50" "VGG19")
+#Datasets
+datasets=("cifar10" "cifar100")
+#Pruning methods
+pruner=("global" "lamp")
+
+
+
+# For over noise level
+# shellcheck disable=SC2068
+for sigma in ${noise[@]}; do
+# For over pruning level
+for pr in ${pruning[@]}; do
+# For over the models
+for model in ${models[@]};do
+# For over  datasets
+for dataset in ${datasets[@]};do
+# For over pruning methods
+for pruner in ${pruner[@]};do
+if [ "${pr}" = "0.8" ];then
+  continue
+fi
+if [ "${pr}" = "0.9" ] && [  "${model}" = "resnet18" ]; then
+ continue
+fi
+echo "${sigma} ${pr} ${model} ${dataset} ${pruner}"
+
+done
+done
+done
+done
+done
