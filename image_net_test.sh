126aab13c9a1a3ca0f7a1ae1288818c4f0976a5c
Elluito <alfredoavendano@hotmail.com>
2023-06-12 20:41:01 +0100

    imagenet test script

---
+++ image_net_test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -l
+# Submission script for serial Python job
+
+# Luis Alfredo Avendano  2021-10-22
+
+# Run from the current directory and with current environment
+#$ -cwd -V
+
+# Ask for some time (hh:mm:ss max of 48:00:00)
+#$ -l h_rt=08:00:00
+
+# ASk for some GPU
+#$ -l coproc_p100=1
+
+# Ask for some memory (by default, 1G, without a request)
+#$ -l h_vmem=8G
+
+module add anaconda
+module add cuda/11.1.1
+conda activate work
+python test_imagenet.sh
