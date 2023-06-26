d9bcfec43cf1c9c94b6c4c91d28e9648fc4773c1
Elluito <alfredoavendano@hotmail.com>
2023-06-26 17:46:30 +0100

    Things

--- main.py
+++ main.py
@@ -7580,7 +7580,7 @@ if __name__ == '__main__':
 
     ### Deterministic pruner vs stochastic pruner based on pruner, dataset, sigma, and pruning rate present on cfg #####
 
-    stochastic_pruning_against_deterministic_pruning(cfg,name="")
+    # stochastic_pruning_against_deterministic_pruning(cfg,name="")
     # stochastic_pruning_global_against_LAMP_deterministic_pruning(cfg)
 
 
