2c5365bfc8b2c3dde3df65e41aebafb4368db8ef
Elluito <alfredoavendano@hotmail.com>
2023-06-26 17:22:55 +0100

    things

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@
 #$ -cwd -V
 
 # Ask for some time (hh:mm:ss max of 48:00:00)
-#$ -l h_rt=01:00:00
+#$ -l h_rt=0:20:00
 
 # ASk for some GPU
-#$ -l coproc_p100=1
+#$ -l coproc_k80=1
 
 # Ask for some memory (by default, 1G, without a request)
-#$ -l h_vmem=12G
+#$ -l h_vmem=8G
 # -pe smp 3
 # Send emails when job starts and ends
 #$ -m e
