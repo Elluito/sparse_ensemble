44fdb004d00b315a8c1f471494456335bfefc9d3
Elluito <alfredoavendano@hotmail.com>
2023-07-17 13:19:17 +0100

    Commit, imagenet. python

--- purge_nobackup.sh
+++ purge_nobackup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #Noise level
 # shellcheck disable=SC2054
-noise=("0.001" "0.003" "0.005")
+noise=("0.001" "0.003" "0.005" "0.0032" "0.0043" "0.0065" "0.0076" "0.0087" "0.0098" "0.011")
 #noise=("0.003")
 #Pruning level
 pruning=("0.8" "0.85" "0.9" "0.95")
@@ -37,8 +37,10 @@ for dataset in ${datasets[@]};do
 #ls -1t "/nobackup/sclaam/gradient_flow_data/${dataset}/stochastic_LAMP/${model}/sigma${sigma}/pr${pr}/"| head -n -10 | xargs -d '\n' rm -rf --
 echo "/nobackup/sclaam/gradient_flow_data/${dataset}/deterministic_GLOBAL/${model}/sigma${sigma}/pr${pr}/"
 ls -1t "/nobackup/sclaam/gradient_flow_data/${dataset}/deterministic_GLOBAL/${model}/sigma0.0/pr${pr}/" | head -n -10 | xargs -d '\n' rm -rf --
+ls "/nobackup/sclaam/gradient_flow_data/${dataset}/deterministic_GLOBAL/${model}/sigma0.0/pr${pr}/"
 echo "/nobackup/sclaam/gradient_flow_data/${dataset}/deterministic_LAMP/${model}/sigma0.0/pr${pr}/"
 ls -1t  "/nobackup/sclaam/gradient_flow_data/${dataset}/deterministic_LAMP/${model}/sigma0.0/pr${pr}/" | head -n -10 | xargs -d '\n' rm -rf --
+ls "/nobackup/sclaam/gradient_flow_data/${dataset}/deterministic_LAMP/${model}/sigma0.0/pr${pr}/"
 
 #ls -1t "/nobackup/sclaam/gradient_flow_data/${dataset}/stochastic_GLOBAL/${model}/sigma${sigma}/pr${pr}/"
 #| head -n -10 | xargs -d '\n' rm -rf --
