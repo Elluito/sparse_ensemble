2f4bcac721eadec6d513ca88117145dbbb7edd86
Elluito <alfredoavendano@hotmail.com>
2023-08-02 15:40:48 +0100

    Mask transfer control

--- run.sh
+++ run.sh
@@ -6,13 +6,13 @@
 #$ -cwd -V
 
 # Ask for some time (hh:mm:ss max of 48:00:00)
-#$ -l h_rt=015:00:00
+#$ -l h_rt=03:00:00
 
 # ASk for some GPU
 #$ -l coproc_p100=1
 
 # Ask for some memory (by default, 1G, without a request)
-#$ -l h_vmem=90G
+#$ -l h_vmem=16G
 # -pe smp 3
 # Send emails when job starts and ends
 #$ -m e
@@ -24,7 +24,7 @@
 module add anaconda
 module add cuda/11.1.1
 conda activate work
-which python
+#which python
 unset GOMP_CPU_AFFINITY
 #nvcc --version
 #python main.py $1 $2 $3 $4 $5 $6
