6c88d2cdd758e7d2744159909c3fab3b38fcbd75
Elluito <alfredoavendano@hotmail.com>
2023-10-18 16:51:37 +0100

    Similarity matrices experiments

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -103,12 +103,13 @@ name_rf_level_p_s3="_seed_3_rf_level_p"
 
 
 
+
 #python main.py -exp 18 -bs 128 --sigma $2 --pruner $3 --architecture $4 --dataset $5 --pruning_rate $6 --modeltype $7 --epochs $8
 
-qsub -N "saving_features_resnet50_rf_levelp_s3_pytorch_no_train" run.sh  "resnet50" "" "pytorch_no_train_1" 0 hub
-qsub -N "saving_features_resnet50_rf_levelp_s3_pytorch_no_train" run.sh  "resnet50" "" "pytorch_no_train_2" 0 hub
-qsub -N "saving_features_resnet50_rf_level0_s3_pytorch_no_train" run.sh  "resnet50" "" "custom_no_train_1" 0 alternative
-qsub -N "saving_features_resnet50_rf_level0_s3_pytorch_no_train" run.sh  "resnet50" "" "custom_no_train_2" 0 alternative
+qsub -N "saving_features_resnet50_rf_levelp_s3_pytorch_no_train" run.sh  "resnet50" "filer" "pytorch_no_train_1" 0 hub
+qsub -N "saving_features_resnet50_rf_levelp_s3_pytorch_no_train" run.sh  "resnet50" "filer" "pytorch_no_train_2" 0 hub
+qsub -N "saving_features_resnet50_rf_level0_s3_pytorch_no_train" run.sh  "resnet50" "filer" "custom_no_train_1" 0 alternative
+qsub -N "saving_features_resnet50_rf_level0_s3_pytorch_no_train" run.sh  "resnet50" "filer" "custom_no_train_2" 0 alternative
 #./run.sh  "resnet50" foreing_trained_models/cifar10/resnet50_pytorch_cifar10_seed_3_test_acc_89.33.pth "${name_rf_level_p_s3}" 0 hub
 #qsub -N "saving_features_resnet50_rf_level1_s1" run.sh  "resnet50" "${rf_level1_s1}" "${name_rf_level1_s1}" 1 alternative
 #qsub -N "saving_features_resnet50_rf_level1_s2" run.sh  "resnet50" "${rf_level1_s2}" "${name_rf_level1_s2}" 1 alternative
@@ -134,6 +135,10 @@ qsub -N "saving_features_resnet50_rf_level0_s3_pytorch_no_train" run.sh  "resnet
 #
 #qsub -N "similarity_tird_seed_pytorch_1_k80" run.sh  "resnet50"  "${name_rf_level_p_s3}" "_seed_2" "${rf_level_p_s3}"
 #./run.sh  "resnet50"  "${name_rf_level_p_s3}" "_seed_2" "${rf_level_p_s3}"
+
+
+
+### Smoothness#######################
 
 #files_names=($name_rf_level0_s1 $name_rf_level1_s1  $name_rf_level2_s1 $name_rf_level3_s1 $name_rf_level4_s1 $name_rf_level_p_s1)
 #files=($rf_level0_s1 $rf_level1_s1  $rf_level2_s1 $rf_level3_s1 $rf_level4_s1 $rf_level_p_s1)
