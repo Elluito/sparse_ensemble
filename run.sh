8f9addd77fcb59c4c0653772f4a369ccffc66f5b
Elluito <alfredoavendano@hotmail.com>
2023-10-18 17:23:24 +0100

    Similarity matrices experiments

--- run.sh
+++ run.sh
@@ -46,11 +46,11 @@ unset KMP_AFFINITY
 
 # To save the representations
 echo "First similarity function"
-python similarity_comparison_architecture.py --experiment 1 --architecture $1 --seedname1 $3 -rfl $4 --modeltype1 $5
+#python similarity_comparison_architecture.py --experiment 1 --architecture $1 --seedname1 $3 -rfl $4 --modeltype1 $5
 #echo "Change type of file"
 #python change_files.py --architecture $1 --modeltype hub --seedname1 $2
 #echo "Compare the new features with seed 1"
-#python similarity_comparison_architecture.py --experiment 2 --architecture $1 --seedname1 $2 --seedname2 $3 --modeltype1 $4 --modeltype2 $5 --filetype1 $6 --filetype2 $7
+python similarity_comparison_architecture.py --experiment 2 --architecture $1 --seedname1 $2 --seedname2 $3 --modeltype1 $4 --modeltype2 $5 --filetype1 $6 --filetype2 $7
 #echo "Compare the new features with seed 2"
 #python similarity_comparison_architecture.py --experiment 2 --architecture $1 --seedname1 $2 --seedname2 "_seed_2"
 
