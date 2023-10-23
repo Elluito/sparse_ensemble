e57ddc552d8965ed1a50e2a65cbc7fd583e67648
Elluito <alfredoavendano@hotmail.com>
2023-10-23 15:20:44 +0100

    hessian information data

--- run.sh
+++ run.sh
@@ -5,12 +5,12 @@
 # Run from the current directory and with current environment
 #$ -cwd -V
 
-# Ask for some time (hh:mm:ss max of 48:00:00)
-#$ -l h_rt=24:00:00
+# Ask for some time (hh:mm:ss max of 00:10:00)
+#$ -l h_rt=00:05:00
 
 
 # ASk for some GPU
-#$ -l coproc_p100=1
+# -l coproc_p100=1
 
 # Ask for some memory (by default, 1G, without a request)
 #$ -l h_vmem=16G
