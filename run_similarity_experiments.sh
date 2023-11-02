96b364f950c084a86cd22ab11cca3b49f818a9f3
Elluito <alfredoavendano@hotmail.com>
2023-11-02 17:47:42 +0000

    Vgg train

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -209,12 +209,12 @@ name_rf_level_p_s3="_seed_3_rf_level_p"
 
  directory=/nobackup/sclaam/checkpoints
  all_level_1_seeds=($(ls $directory | grep -i "vgg19.*_level_1_.*" |cut -d_ -f4 |uniq))
- echo $all_level_1_seeds
- all_level_2_seeds=$(ls $directory | grep -i "vgg19.*_level_2_.*" |cut -d_ -f4 |uniq)
+# echo $all_level_1_seeds
+ all_level_2_seeds=($(ls $directory | grep -i "vgg19.*_level_2_.*" |cut -d_ -f4 |uniq))
  echo $all_level_2_seeds
- all_level_3_seeds=$(ls $directory | grep -i "vgg19.*_level_3_.*" |cut -d_ -f4 |uniq)
+ all_level_3_seeds=($(ls $directory | grep -i "vgg19.*_level_3_.*" |cut -d_ -f4 |uniq))
  echo $all_level_3_seeds
- all_level_4_seeds=$(ls $directory | grep -i "vgg19.*_level_4_.*" |cut -d_ -f4 |uniq)
+ all_level_4_seeds=($(ls $directory | grep -i "vgg19.*_level_4_.*" |cut -d_ -f4 |uniq))
  echo $all_level_4_seeds
 
 
