22952555e0b5eb7931bdad11012be35e945549da
Elluito <alfredoavendano@hotmail.com>
2023-11-13 13:26:17 +0000

    Change seed names

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -308,13 +308,14 @@ name_rf_level_p_s3="_seed_3_rf_level_p"
 ###############################################################################
 
 directory=/nobackup/sclaam/checkpoints
- all_level_1_seeds=($(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_1_.*" |cut -d_ -f5 |uniq))
+ all_level_1_seeds=$(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_1_.*" |cut -d_ -f5 |uniq)
  echo $all_level_1_seeds
- all_level_2_seeds=($(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_2_.*" |cut -d_ -f5 |uniq))
+ echo "\n"
+ all_level_2_seeds=$(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_2_.*" |cut -d_ -f5 |uniq)
  echo $all_level_2_seeds
- all_level_3_seeds=($(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_3_.*" |cut -d_ -f5 |uniq))
+ all_level_3_seeds=$(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_3_.*" |cut -d_ -f5 |uniq)
  echo $all_level_3_seeds
- all_level_4_seeds=($(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_4_.*" |cut -d_ -f5 |uniq))
+ all_level_4_seeds=$(ls $directory | grep -i "vgg19_normal_tiny_imagenet.*_level_4_.*" |cut -d_ -f5 |uniq)
  echo $all_level_4_seeds
 #
 #
