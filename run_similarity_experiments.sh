5a9beb27d53eca5cdd56c1737922562a8de113ab
Elluito <alfredoavendano@hotmail.com>
2023-12-15 15:57:34 +0000

    update

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -825,53 +825,53 @@ done
 
 
 
+
+declare -a list_to_use=("${all_level_6_seeds[@]}")
 #
-#declare -a list_to_use=("${all_level_6_seeds[@]}")
-##
-#max=${#list_to_use[@]}                                  # Take the length of that array
-##
-#echo $max
-##
-#for ((idxA=0; idxA<max; idxA++)); do # iterate idxA from 0 to length
-#echo "${directory}/.*${list_to_use[$idxA]}\.\*"
-#file_names=($(ls $directory | grep -i ".*${list_to_use[$idxA]}.*.pth"))
-#echo $file_names
-#echo ${#file_names[@]}                                  # Take the length of that array
-#echo $idxA
+max=${#list_to_use[@]}                                  # Take the length of that array
 #
-#for pathname in  "${file_names[@]}"; do
-#replace_string="seed_${idxA}"
-#thing="${pathname/"${list_to_use[$idxA]}"/$replace_string}"
-#  echo "${thing}"
-##  mv -i "${directory}/${pathname}" "${directory}/${thing}"
-#done
-#done
+echo $max
 #
+for ((idxA=0; idxA<max; idxA++)); do # iterate idxA from 0 to length
+echo "${directory}/.*${list_to_use[$idxA]}\.\*"
+file_names=($(ls $directory | grep -i ".*${list_to_use[$idxA]}.*.pth"))
+echo $file_names
+echo ${#file_names[@]}                                  # Take the length of that array
+echo $idxA
+
+for pathname in  "${file_names[@]}"; do
+replace_string="seed_${idxA}"
+thing="${pathname/"${list_to_use[$idxA]}"/$replace_string}"
+  echo "${thing}"
+#  mv -i "${directory}/${pathname}" "${directory}/${thing}"
+done
+done
+
+
+
+
+
+
+declare -a list_to_use=("${all_level_7_seeds[@]}")
 #
+max=${#list_to_use[@]}                                  # Take the length of that array
 #
+echo $max
 #
-#
-#
-#declare -a list_to_use=("${all_level_7_seeds[@]}")
-##
-#max=${#list_to_use[@]}                                  # Take the length of that array
-##
-#echo $max
-##
-#for ((idxA=0; idxA<max; idxA++)); do # iterate idxA from 0 to length
-#echo "${directory}/.*${list_to_use[$idxA]}\.\*"
-#file_names=($(ls $directory | grep -i ".*${list_to_use[$idxA]}.*.pth"))
-#echo $file_names
-#echo ${#file_names[@]}                                  # Take the length of that array
-#echo $idxA
-#
-#for pathname in  "${file_names[@]}"; do
-#replace_string="seed_${idxA}"
-#thing="${pathname/"${list_to_use[$idxA]}"/$replace_string}"
-#  echo "${thing}"
-##  mv -i "${directory}/${pathname}" "${directory}/${thing}"
-#done
-#done
+for ((idxA=0; idxA<max; idxA++)); do # iterate idxA from 0 to length
+echo "${directory}/.*${list_to_use[$idxA]}\.\*"
+file_names=($(ls $directory | grep -i ".*${list_to_use[$idxA]}.*.pth"))
+echo $file_names
+echo ${#file_names[@]}                                  # Take the length of that array
+echo $idxA
+
+for pathname in  "${file_names[@]}"; do
+replace_string="seed_${idxA}"
+thing="${pathname/"${list_to_use[$idxA]}"/$replace_string}"
+  echo "${thing}"
+#  mv -i "${directory}/${pathname}" "${directory}/${thing}"
+done
+done
 
 
 ########################################################################################################################
