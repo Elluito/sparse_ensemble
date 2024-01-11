285dfa85ec1df2ea8cc72a61e4ff1bc38c1a658e
Elluito <alfredoavendano@hotmail.com>
2024-01-11 15:23:53 +0000

    update

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -541,7 +541,7 @@ for ((idxB=0; idxB<levels_max; idxB++));do              # iterate idxB from 0 to
 #qsub -N "${model}_${dataset}pruning_summary_level_1_${pruning_rates[$idxA]}" run.sh "${model}" "${dataset}" "2" "1" "normal" "${directory}" "${pruning_rates[$idxA]}" "1"
 ##echo "${model}" "${dataset}" "2" "1" "normal" "${directory}" "pruning" "${list_to_use[$idxB]}"
 
-qsub -l h_rt=1:00:00-N "${model}_${dataset}_pruning_summary_level_${rf_levels[$idxB]}" run.sh "${model}" "${dataset}" "2" "${rf_levels[$idxB]}" "normal" "${directory}" #"${[$idxB]}" "3"
+qsub -l h_rt=1:00:00 -N "${model}_${dataset}_pruning_summary_level_${rf_levels[$idxB]}" run.sh "${model}" "${dataset}" "2" "${rf_levels[$idxB]}" "normal" "${directory}" #"${[$idxB]}" "3"
 done
 ##done
 
