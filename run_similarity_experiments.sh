378762d20e51ab2a38458be3d604f2ed4913e32d
Elluito <alfredoavendano@hotmail.com>
2024-01-26 15:05:47 +0000

    update

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -244,8 +244,8 @@ max=${#files_level[@]}                                  # Take the length of tha
 for ((idxA=0; idxA<max; idxA++)); do              # iterate idxA from 0 to length
 #  qsub -N "features_${files_level[$idxA]}_seed_${file_seed[$idxA]}" run.sh  "resnet50" "/nobackup/sclaam/checkpoints/resnet50_normal_cifar10_seed_${file_seed[$indxA]}_rf_level_${files_level[$indxA]}_initial_weights.pth" "_no_train_seed_${file_seed[$idxA]}_rf_level_${files_level[$idxA]}"  "${files_level[$idxA]}" "alternative"
 #  qsub -l coproc_p100=1 -l h_rt=01:00:00 -N "features_7_seed_${list_to_use_seeds[$idxA]}" run.sh  "resnet50" "${directory}/${list_to_use_files[$idxA]}" "_trained_seed_${list_to_use_seeds[$idxA]}_rf_level_7"  "7" "alternative"
-qsub -l coproc_p100=1 -l h_rt=20:00:00 -N "logistic_features_train_7_seed_${list_to_use_seeds[$idxA]}" run.sh  "resnet50" "${directory}/${list_to_use_files[$idxA]}" "trained_seed_${list_to_use_seeds[$idxA]}_rf_level_7" "7" "alternative" "1"
-qsub -l coproc_p100=1 -l h_rt=20:00:00 -N "logistic_features_test_7_seed_${list_to_use_seeds[$idxA]}" run.sh "resnet50" "${directory}/${list_to_use_files[$idxA]}" "trained_seed_${list_to_use_seeds[$idxA]}_rf_level_7" "7" "alternative" "0"
+qsub -l coproc_p100=1 -l h_rt=20:00:00 -N "logistic_features_train_7_seed_${list_to_use_seeds[$idxA]}" run.sh  "resnet50" "${directory}/${list_to_use_files[$idxA]}" "_trained_seed_${list_to_use_seeds[$idxA]}_rf_level_7" "7" "alternative" "1"
+qsub -l coproc_p100=1 -l h_rt=20:00:00 -N "logistic_features_test_7_seed_${list_to_use_seeds[$idxA]}" run.sh "resnet50" "${directory}/${list_to_use_files[$idxA]}" "_trained_seed_${list_to_use_seeds[$idxA]}_rf_level_7" "7" "alternative" "0"
 #  qsub -l coproc_p100=1 -l h_rt=15:00:00 -N "Similarity${list_to_use_seeds[$idxA]}" run.sh  "resnet50" "${directory}/${list_to_use_files[$idxA]}" "trained_seed_${list_to_use_seeds[$idxA]}_rf_level_7"  "7" "alternative"
 #echo "solution: ${list_to_use_files[$idxA]} , seed in the same index: ${list_to_use_seeds[$idxA]}"
   if [ $idxA -gt 0 ]
