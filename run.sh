d451a659a5569cf3d18814291547fc21e4f10a3c
Elluito <alfredoavendano@hotmail.com>
2024-01-28 21:22:48 +0000

    update

--- run.sh
+++ run.sh
@@ -66,7 +66,7 @@ python similarity_comparison_architecture.py --experiment 3 --architecture $1 --
 #        Train logistic regression on a particular set of features
 #############################################################
 
-python similarity_comparison_architecture.py --experiment 5 --architecture $1 --solution $2 --seedname1 $3 -rfl $4 --modeltype1 $5 --layer_index $6
+#python similarity_comparison_architecture.py --experiment 5 --architecture $1 --solution $2 --seedname1 $3 -rfl $4 --modeltype1 $5 --layer_index $6
 #############################################################
 #       Calculate the similarity of two seeds
 #############################################################
