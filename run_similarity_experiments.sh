45f63ea9c66d2affe55a82fa2e508a762525b397
Elluito <alfredoavendano@hotmail.com>
2024-04-25 17:15:41 +0100

    Small models addition to training code

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -113,6 +113,7 @@ qsub -l h_rt=24:00:00 -l coproc_k80=1  -N "training_Level_4_vgg_small_small_imag
 #
 qsub -l h_rt=24:00:00 -l coproc_k80=1  -N "training_Level_1_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 1 "normal" 100 "recording_100" 1 1
 qsub -l h_rt=24:00:00 -l coproc_k80=1  -N "training_Level_3_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 3 "normal" 100 "recording_100" 1 1
+qsub -l h_rt=24:00:00 -l coproc_k80=1  -N "training_Level_5_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 5 "normal" 100 "recording_100" 1 1
 #qsub -l h_rt=12:00:00 -l coproc_v100=1  -N "training_Level_6_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 1 6 "normal" 200 "recording" 1 1
 #qsub -l h_rt=6:00:00 -l coproc_p100=1  -N "training_Level_7_resnet_small_cifar10" run.sh "resnet_small" "cifar10" 2 7 "normal" 400 "recording_400" 1 1
 qsub -l h_rt=24:00:00 -l coproc_k80=1  -N "training_Level_7_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 7 "normal" 100 "recording_100" 1 1
