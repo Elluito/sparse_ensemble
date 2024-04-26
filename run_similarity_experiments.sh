ce9b3cbb919a602cfe7786cd0935c62ead1afceb
Elluito <alfredoavendano@hotmail.com>
2024-04-26 16:47:49 +0100

    Small models addition to training code

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -105,18 +105,18 @@
 #qsub -l h_rt=44:00:00 -l coproc_k80=1 -N "Kfac_optim_rf_7_tiny_imagenet" run.sh "tiny_imagenet" "resnet50" "7" "normal" "kfac_optimiser_2"
 
 
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_1_vgg_small_small_imagenet" run.sh "vgg_small" "small_imagenet" 2 1 "normal" 100 "recording_100" 1 1
+qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_1_vgg_small_imagenet" run.sh "vgg19" "small_imagenet" 2 1 "normal" 200 "recording_200" 1 1
 #qsub -l h_rt=24:00:00 -l coproc_k80=1  -N "training_Level_2_vgg_small_small_imagenet" run.sh "vgg_small" "small_imagenet" 2 2 "normal" 100 "recording_100" 1 1
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_3_vgg_small_small_imagenet" run.sh "vgg_small" "small_imagenet" 2 3 "normal" 100 "recording_100" 1 1
+qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_3_vgg_small_imagenet" run.sh "vgg19" "small_imagenet" 2 3 "normal" 200 "recording_200" 1 1
 #qsub -l h_rt=12:00:00 -l coproc_p100=1  -N "training_Level_3_vgg_small_cifar10" run.sh "vgg_small" "small_imagenet" 2 3 "normal" 200 "recording" 1 1
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_4_vgg_small_small_imagenet" run.sh "vgg_small" "small_imagenet" 2 4 "normal" 100 "recording_100" 1 1
+qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_4_vgg_small_small_imagenet" run.sh "vgg19" "small_imagenet" 2 4 "normal" 200 "recording_200" 1 1
 #
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_1_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 1 "normal" 100 "recording_100" 1 1
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_3_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 3 "normal" 100 "recording_100" 1 1
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_5_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 5 "normal" 100 "recording_100" 1 1
+#qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_1_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 1 "normal" 200 "recording_200" 1 1
+qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_3_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 3 "normal" 200 "recording_200" 1 1
+qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_5_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 5 "normal" 200 "recording_200" 1 1
 #qsub -l h_rt=12:00:00 -l coproc_v100=1  -N "training_Level_6_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 1 6 "normal" 200 "recording" 1 1
 #qsub -l h_rt=6:00:00 -l coproc_p100=1  -N "training_Level_7_resnet_small_cifar10" run.sh "resnet_small" "cifar10" 2 7 "normal" 400 "recording_400" 1 1
-qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_7_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 7 "normal" 100 "recording_100" 1 1
+qsub -l h_rt=45:00:00 -l coproc_k80=1  -N "training_Level_7_resnet_small_imagenet" run.sh "resnet_small" "small_imagenet" 2 7 "normal" 200 "recording_200" 1 1
 #qsub -l h_rt=30:00:00 -l coproc_p100=1 -N "hyper_parameter_optim_second_order" run.sh
 
 #type="one_shot"
--- test_imagenet.py
+++ test_imagenet.py
@@ -296,8 +296,8 @@ def load_small_imagenet(args):
                                      std=[0.229, 0.224, 0.225])
 
     transform_test = transforms.Compose([
-        transforms.Resize(161),
-        transforms.CenterCrop(1),
+        transforms.Resize(392),
+        transforms.CenterCrop(360),
         transforms.ToTensor(),
         transforms.Normalize([0.4824, 0.4495, 0.3981], [0.2301, 0.2264, 0.2261]),
     ])
