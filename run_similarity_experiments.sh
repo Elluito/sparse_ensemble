a2a22e7d7df3232cc9d101715a8f56422f66a084
Elluito <alfredoavendano@hotmail.com>
2024-05-10 21:30:12 +0100

    Small models addition to training code

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -500,14 +500,15 @@
 
 
 #
-model="resnet_small"
+#model="resnet_small"
+model="vgg19"
 dataset="small_imagenet"
 directory=/nobackup/sclaam/checkpoints
 
 ##seeds=(0 1 2)
 pruning_rates=("0.5" "0.6" "0.7" "0.8" "0.9")
-#rf_levels=(2 3 4)
-rf_levels=(3 5 7)
+rf_levels=(2 3 4)
+#rf_levels=(3 5 7)
 levels_max=${#rf_levels[@]}                                  # Take the length of that array
 #seeds_per_level=${#list_to_use[@]}                            # Take the length of that array
 number_pruning_rates=${#pruning_rates[@]}                            # Take the length of that array
