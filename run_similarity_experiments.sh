af88df91a37643699a9a24454a7d2ab75a8fa629
Elluito <alfredoavendano@hotmail.com>
2024-05-10 21:29:36 +0100

    Small models addition to training code

--- run_similarity_experiments.sh
+++ run_similarity_experiments.sh
@@ -505,7 +505,7 @@ dataset="small_imagenet"
 directory=/nobackup/sclaam/checkpoints
 
 ##seeds=(0 1 2)
-pruning_rates=("0.5" "0.6" "0.7" "0.8 0.9")
+pruning_rates=("0.5" "0.6" "0.7" "0.8" "0.9")
 #rf_levels=(2 3 4)
 rf_levels=(3 5 7)
 levels_max=${#rf_levels[@]}                                  # Take the length of that array
@@ -517,7 +517,7 @@ for ((idxB=0; idxB<levels_max; idxB++));do              # iterate idxB from 0 to
 #
 #qsub -N "${model}_${dataset}pruning_fine_tuning_summary_level_1_${pruning_rates[$idxB]}" run.sh "${model}" "${dataset}" "2" "1" "normal" "${directory}" "pruning" "${list_to_use[$idxB]}" "0.9" "2"
 
-qsub -l coproc_v100=1 -l h_rt=00:20:00   -N "${model}_${dataset}_pruning_summary_level_${rf_levels[$idxB]}_${pruning_rates[$idxA]}" run.sh "${model}" "${dataset}" "2" "${rf_levels[$idxa]}" "normal" "${directory}" "${pruning_rates[$idxA]}" "1"
+qsub -l coproc_v100=1 -l h_rt=00:20:00 -N "${model}_${dataset}_pruning_summary_level_${rf_levels[$idxB]}_${pruning_rates[$idxA]}" run.sh "${model}" "${dataset}" "2" "${rf_levels[$idxa]}" "normal" "${directory}" "${pruning_rates[$idxA]}" "1"
 #
 ##./run.sh "${model}" "${dataset}" "2" "1" "normal" "${directory}" "pruning" "${list_to_use[$idxB]}" "0.5" "1"
 done
