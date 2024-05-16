7ecb7863b3e6a2d2de367cbc45729296044e726a
Elluito <alfredoavendano@hotmail.com>
2024-05-16 14:24:34 +0100

    Small models addition to training code

--- slurm _handler.sh
+++ slurm _handler.sh
@@ -18,5 +18,6 @@ sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL 
 sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --job-name="sam_optim_rf_k6_cifar10_rs" slurm_run.sh "cifar10" "resnet50" "k6" "normal" "sam_optim_hyper" "2"
 sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --job-name="sam_optim_rf_k7_cifar10_rs" slurm_run.sh "cifar10" "resnet50" "k7" "normal" "sam_optim_hyper" "2"
 sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --job-name="sam_optim_rf_k8_cifar10_rs" slurm_run.sh "cifar10" "resnet50" "k8" "normal" "sam_optim_hyper" "2"
+
 sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --job-name="sam_optim_rf_k9_cifar10_rs" slurm_run.sh "cifar10" "resnet50" "k9" "normal" "sam_optim_hyper" "2"
 
