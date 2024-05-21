c9cab521c058548e932aeb9dc08ff37bdf1ce16f
Elluito <alfredoavendano@hotmail.com>
2024-05-21 12:45:20 +0100

    Small models addition to training code

--- run.sh
+++ run.sh
@@ -181,19 +181,19 @@ unset KMP_AFFINITY
 #############################################################
 
 # python Second_order_Receptive_field.py --experiment 2 --optimiser $1 # --save_folder "$HOME/checkpoints"
-
+#
 # python Second_order_Receptive_field.py --experiment 2 --optimiser $1 --save_folder "$HOME/checkpoints"
 
-#  if [ $6 -eq 1 ]
-#  then
-#    # KFac
-#python Second_order_Receptive_field.py --lr "0.01" --momentum "0.5" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 32 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "kfac" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
-#
-#  fi
-#
-#  if [ $6 -eq 2 ]
-#    # SAM
-#  then
-#python Second_order_Receptive_field.py --lr "0.1" --momentum "0.7" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 128 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "sam" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
-#  fi
-python hao_models_pruning_test.py --experiment 1
+  if [ $6 -eq 1 ]
+  then
+    # KFac
+python Second_order_Receptive_field.py --lr "0.01" --momentum "0.5" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 32 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "kfac" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+
+  fi
+
+  if [ $6 -eq 2 ]
+    # SAM
+  then
+python Second_order_Receptive_field.py --lr "0.1" --momentum "0.7" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 128 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "sam" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+  fi
+#python hao_models_pruning_test.py --experiment 1
