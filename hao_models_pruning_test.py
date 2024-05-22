f19bfaf1c6dbe99ab2024d7412e2b0e0ba15c341
Elluito <alfredoavendano@hotmail.com>
2024-05-22 16:44:04 +0100

    small models addition to training code

--- hao_models_pruning_test.py
+++ hao_models_pruning_test.py
@@ -538,7 +538,7 @@ def test(net, use_cuda, testloader, one_batch=False, verbose=2, count_flops=Fals
             if batch_idx ==0:
                 print("In the data loader loop")
             if use_cuda:
-                targets.type(torch.LongTensor)
+                target=targets.type(torch.LongTensor)
                 inputs, targets = inputs.cuda(), targets.cuda()
             if batch_idx ==0:
                 print("before forward method")
