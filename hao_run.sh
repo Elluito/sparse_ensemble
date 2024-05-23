b604f10dd284d3af849fbb8af8f4aa00949612b4
Elluito <alfredoavendano@hotmail.com>
2024-05-23 12:10:52 +0100

    small models addition to training code

--- hao_run.sh
+++ hao_run.sh
@@ -4,7 +4,7 @@
 
 slurm_run.sh "resnet34"
 slurm_run.sh "legacy_seresnet34"
-slurm_run.sh "skresnet34"
-slurm_run.sh "mobilenetv2"
+#slurm_run.sh "skresnet34"
+#slurm_run.sh "mobilenetv2"
 slurm_run.sh "mobilenetv3"
 slurm_run.sh "efficientnet"
