3485c77888153e724e6fdf190543de56f93b4cb6
Elluito <alfredoavendano@hotmail.com>
2024-06-03 20:54:49 +0100

    Test backward pass in JADE

---
+++ slurm_2nd_order.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=00:09:00
+
+# set name of job
+#SBATCH --job-name=pytorch_test
+
+#SBATCH --error=pytorch_test.err
+
+#SBATCH --output=pytorch_test.output
+
+# set partition (devel, small, big)
+
+#SBATCH --partition=small
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+# send mail to this address
+#SBATCH --mail-user=sclaam@leeds.ac.uk
+#module load pytorch
+
+which python
+
+#python train_CIFAR10.py --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5
+
+#python Second_order_Receptive_field.py --experiment 2 --optimiser "kfac"
+#
+#python Second_order_Receptive_field.py --experiment 2 --optimiser "sam"
+
+  if [ $6 -eq 1 ]
+  then
+    # KFac
+python Second_order_Receptive_field.py --lr "0.01" --momentum "0.5" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 32 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "kfac" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+
+  fi
+
+  if [ $6 -eq 2 ]
+    # SAM
+  then
+python Second_order_Receptive_field.py --lr "0.1" --momentum "0.7" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 128 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "sam" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+  fi
+#
