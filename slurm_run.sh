5f5a6cdb77edc4530c15f50a0cff37ad02358555
Elluito <alfredoavendano@hotmail.com>
2024-06-06 18:05:59 +0100

    Test backward pass in JADE

--- slurm_run.sh
+++ slurm_run.sh
@@ -101,4 +101,4 @@ python -c "import os; print(os.environ)"
 #     One shot with specific pruning rate results
 #############################################################
 
-python prune_models.py  --ffcv --name "recording_200" --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --folder $6 --pruning_rate $7 --experiment $8
+#python prune_models.py  --ffcv --name "recording_200" --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --folder $6 --pruning_rate $7 --experiment $8
