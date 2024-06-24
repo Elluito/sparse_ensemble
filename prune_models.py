2e2490291c3a250880ec02aa8a41f3ec19485296
Elluito <alfredoavendano@hotmail.com>
2024-06-24 10:49:01 +0100

    test backward pass in jade

--- prune_models.py
+++ prune_models.py
@@ -849,8 +849,8 @@ def main(args):
         df2 = pd.DataFrame({"layer_names": weight_names, "pr": pruning_rates_per_layer})
         print("Seed from file {}".format(seed_from_file1))
         df2.to_csv(
-            "{}_level_{}_seed_{}_{}_pruning_rates_global_pr_{}.csv".format(args.model, args.RF_level, seed_from_file,
-                                                                           args.dataset, args.pruning_rate),
+            "{}_level_{}_seed_{}_{}_{}_pruning_rates_global_pr_{}.csv".format(args.model, args.RF_level, seed_from_file,
+                                                                           args.dataset, args.name,args.pruning_rate),
             index=False)
 
         print("Done")
@@ -862,7 +862,7 @@ def main(args):
                        "Dense Accuracy": dense_accuracy_list,
                        "Pruned Accuracy": pruned_accuracy_list,
                        })
-    df.to_csv("RF_{}_{}_{}_{}_one_shot_summary.csv".format(args.model, args.RF_level, args.dataset, args.pruning_rate),
+    df.to_csv("RF_{}_{}_{}_{}_{}_one_shot_summary.csv".format(args.model, args.RF_level, args.dataset, args.pruning_rate,args.name),
               index=False)
 
 
