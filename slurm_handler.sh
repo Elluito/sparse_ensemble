9e8ac8add299d3e407d16149ae6019ca5537c8d6
Elluito <alfredoavendano@hotmail.com>
2024-07-01 20:01:11 +0100

    test backward pass in jade

--- slurm_handler.sh
+++ slurm_handler.sh
@@ -68,7 +68,7 @@ grad_clip=0.4
 
 #sbatch --nodes=1 --time=96:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="resume_small_resnet_lv_3_small_imagenet.err" --gres=gpu:1 --output="resume_small_resnet_lv_3_small_imagenet.output"  --job-name="resume_small_resnet_lv_3_small_imagenet" slurm_run.sh "resnet_small" "small_imagenet" 4 3 "normal" 200 "recording_200" 1 1 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/checkpoints/resnet_small_normal_small_imagenet_seed.0_rf_level_3_recording_200_test_acc_51.46.pth"
 #sbatch --nodes=1 --time=96:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="resume_small_resnet_lv_5_small_imagenet.err" --gres=gpu:1 --output="resume_small_resnet_lv_5_small_imagenet.output"  --job-name="resume_small_resnet_lv_5_small_imagenet" slurm_run.sh "resnet_small" "small_imagenet" 4 5 "normal" 200 "recording_200" 1 1 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/checkpoints/resnet_small_normal_small_imagenet_seed.0_rf_level_5_recording_200_test_acc_54.11.pth"
-sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="resume_small_resnet_lv_6_small_imagenet.err" --gres=gpu:1 --output="resume_small_resnet_lv_6_small_imagenet.output"  --job-name="resume_small_resnet_lv_6_small_imagenet" slurm_run_ffcv.sh "resnet_small" "small_imagenet" 4 6 "normal" 200 "recording_200_ffcv" 1 1  "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/checkpoints/resnet_small_normal_small_imagenet_1719238673.8039715_rf_level_6_recording_200_ffcv_test_acc_45.34.pth"
+#sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="resume_small_resnet_lv_6_small_imagenet.err" --gres=gpu:1 --output="resume_small_resnet_lv_6_small_imagenet.output"  --job-name="resume_small_resnet_lv_6_small_imagenet" slurm_run_ffcv.sh "resnet_small" "small_imagenet" 4 6 "normal" 200 "recording_200_ffcv" 1 1  "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/checkpoints/resnet_small_normal_small_imagenet_1719238673.8039715_rf_level_6_recording_200_ffcv_test_acc_45.34.pth"
 
 
 
@@ -135,52 +135,53 @@ sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user
 ######################## Pruning one shot results ##############################
 ################################################################################################
 
-#run_pruning() {
-#model=$1
-#dataset=$2
-#directory=$3
-#data_folder=$4
-#name=$5
-#ffcv=$6
-#ffcv_train=$7
-#ffcv_val=$8
-##echo "model ${model} and dataset ${dataset}"
-#
-#pruning_rates=("0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9")
-## For resnet18
-#
-#  if [ "${9}" -gt 0 ]
-#  then
-##      rf_levels=("5" "7" "8" "9")
+run_pruning() {
+model=$1
+dataset=$2
+directory=$3
+data_folder=$4
+name=$5
+ffcv=$6
+ffcv_train=$7
+ffcv_val=$8
+#echo "model ${model} and dataset ${dataset}"
+
+pruning_rates=("0.3" "0.4" "0.5" "0.6" "0.7" "0.8" "0.9")
+# For resnet18
+
+  if [ "${9}" -gt 0 ]
+  then
+      rf_levels=("5" "7" "8" "9")
 #       rf_levels=("10")
-#
-#  else
-#        rf_levels=("3" "4")
-#  fi
-#
-#levels_max=${#rf_levels[@]}                                  # Take the length of that array
-#number_pruning_rates=${#pruning_rates[@]}                            # Take the length of that array
-#for ((idxA=0; idxA<number_pruning_rates; idxA++)); do                # iterate idxA from 0 to length
-#for ((idxB=0; idxB<levels_max; idxB++));do              # iterate idxB from 0 to length
-#
-#
-#if [ "${ffcv}" -gt 0 ]
-#  then
-#  echo "Use FFCV"
-#sbatch --nodes=1 --time=03:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk  --error="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_ffcv.err" --output="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_ffcv.out" --job-name="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_ffcv"   slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}" DATA_FOLDER="${data_folder}"
-## slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}"
-#
-##./slurm_pruning_run.sh FFCV=0 NAME=recording_200_no_ffcv MODEL=resnet_small DATASET=small_imagenet NUMW=4 RFL=10 TYPE=normal FOLDER=$HOME/checkpoints PR=0.6 EXPERIMENT=1
-#else
-#  echo "Don't use FFCV"
-#sbatch --nodes=1 --time=03:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk  --error="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_no_ffcv.err" --output="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_no_ffcv.out" --job-name="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_no_ffcv" slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 DATA_FOLDER="${data_folder}"
-##   slurm_run.sh  "${model}" "${dataset}" 4  "${rf_levels[$idxB]}" "normal" "${directory}" "${pruning_rates[$idxA]}" 1
-##slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1
-#  fi
-##echo "${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning in directory ${directory}"
-#done
-#done
-#}
-#run_pruning "resnet_small" "small_imagenet" "${HOME}/checkpoints" "${HOME}/datasets" "recording_200_no_ffcv" 0 "no_set" "no_set" 0
-#run_pruning "resnet_small" "small_imagenet" "${HOME}/checkpoints" "${HOME}/datasets" "recording_200_ffcv" 0 "no_set" "no_set" 1
+
+  else
+        rf_levels=("3" "4" "10")
+  fi
+
+levels_max=${#rf_levels[@]}                                  # Take the length of that array
+number_pruning_rates=${#pruning_rates[@]}                            # Take the length of that array
+for ((idxA=0; idxA<number_pruning_rates; idxA++)); do                # iterate idxA from 0 to length
+for ((idxB=0; idxB<levels_max; idxB++));do              # iterate idxB from 0 to length
+
+
+if [ "${ffcv}" -gt 0 ]
+  then
+  echo "Use FFCV"
+sbatch --nodes=1 --time=03:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk  --error="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_ffcv.err" --output="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_ffcv.out" --job-name="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_ffcv"   slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}" DATA_FOLDER="${data_folder}"
+# slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}"
+
+#./slurm_pruning_run.sh FFCV=0 NAME=recording_200_no_ffcv MODEL=resnet_small DATASET=small_imagenet NUMW=4 RFL=10 TYPE=normal FOLDER=$HOME/checkpoints PR=0.6 EXPERIMENT=1
+else
+  echo "Don't use FFCV"
+sbatch --nodes=1 --time=03:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk  --error="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_no_ffcv.err" --output="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_no_ffcv.out" --job-name="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning_no_ffcv" slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 DATA_FOLDER="${data_folder}"
+#   slurm_run.sh  "${model}" "${dataset}" 4  "${rf_levels[$idxB]}" "normal" "${directory}" "${pruning_rates[$idxA]}" 1
+#slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1
+  fi
+#echo "${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning in directory ${directory}"
+done
+done
+}
+run_pruning "resnet_small" "small_imagenet" "${HOME}/checkpoints" "${HOME}/datasets" "recording_200_no_ffcv" 1 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" 0
+#run_pruning "resnet_small" "small_imagenet" "${HOME}/checkpoints" "${HOME}/datasets" "recording_200_no_ffcv" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" 0
+#run_pruning "resnet_small" "small_imagenet" "${HOME}/checkpoints" "${HOME}/datasets" "recording_200_ffcv" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" 1
 #run_pruning "resnet_small" "small_imagenet" "${HOME}/checkpoints" 0
