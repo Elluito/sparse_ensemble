ab661779d30f9f5f2f6aab8ffe40fd9565c2d697
Elluito <alfredoavendano@hotmail.com>
2024-07-26 13:58:44 +0100

    Test

--- truncated_models/small_models.py
+++ truncated_models/small_models.py
@@ -114,15 +114,17 @@ class small_truncated_Bottleneck(nn.Module):
 
     def forward(self, x):
         out = self.relu(self.bn1(self.conv1(x)))
+        pred1 = self.fc1(out)
         out = self.bn2(self.conv2(out))
         # out = self.bn3(self.conv3(out))
         out = out + self.shortcut(x)
         out = self.relu(out)
-        return out
+        pred2 = self.fc2(out)
+        return out, pred1, pred2
 
 
 class ResNet(nn.Module):
-    def __init__(self, block: typing.Union[small_BasicBlock, small_Bottleneck], num_blocks, num_classes=10,
+    def __init__(self, block: typing.Union[small_truncated_BasicBlock, small_truncated_Bottleneck], num_blocks, num_classes=10,
                  fixed_points=None):
         super(ResNet, self).__init__()
         self.in_planes = 64
@@ -168,7 +170,7 @@ class ResNet(nn.Module):
 
 
 class small_truncated_ResNetRF(nn.Module):
-    def __init__(self, block: typing.Union[small_BasicBlock, small_Bottleneck], num_blocks, num_classes=10,
+    def __init__(self, block: typing.Union[small_truncated_BasicBlock, small_truncated_Bottleneck], num_blocks, num_classes=10,
                  multiplier=1,
                  fixed_points=None,
                  RF_level=1):
@@ -211,6 +213,7 @@ class small_truncated_ResNetRF(nn.Module):
         # if self.fix_points is not None:
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
+
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -235,11 +238,11 @@ class small_truncated_ResNetRF(nn.Module):
         out = self.relu(self.bn1(self.conv1(x)))
         # if self.rf_level != 1:
         out = self.maxpool(out)
-        out = self.layer1(out)
+        out, pred11, pred12 = self.layer1(out)
         # print("shape layer1: {}".format(out.shape))
-        out = self.layer2(out)
+        out ,pred21 ,pred22 = self.layer2(out)
         # print("shape layer2: {}".format(out.shape))
-        out = self.layer3(out)
+        out , pred31 ,pred32 = self.layer3(out)
         # print("shape layer3: {}".format(out.shape))
         # out = self.layer4(out)
         out = self.avgpool(out)
@@ -447,7 +450,7 @@ def test_models():
         #     print("Receptive field of VGG")
         #     print(vgg_rf)
 
-        resnet_net = small_truncated_ResNetRF(small_BasicBlock, num_blocks=[1, 1, 1, 1], num_classes=10, RF_level=i)
+        resnet_net = small_truncated_ResNetRF(small_truncated_BasicBlock, num_blocks=[1, 1, 1, 1], num_classes=10, RF_level=i)
 
         # y_resnet = resnet_net(x)
         # input_names = ['Image']
