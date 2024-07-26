6c963dacdf5e32fe6f29812b87a089732b9c5579
Elluito <alfredoavendano@hotmail.com>
2024-07-26 11:39:15 +0100

    Test

--- slurm_handler.sh
+++ slurm_handler.sh
@@ -291,8 +291,8 @@ done
 #run_soup_stochastic resnet18 cifar100 1
 
 
-run_soup_stochastic resnet50 cifar10 1
-#run_soup_stochastic resnet50 cifar100 1
+#run_soup_stochastic resnet50 cifar10 1
+run_soup_stochastic resnet50 cifar100 1
 #run_soup_stochastic vgg19 cifar10 1
 #run_soup_stochastic vgg19 cifar100 1
 
