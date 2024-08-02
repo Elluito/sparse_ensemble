1c87e5881c7dce237e2d795561b3237578469bdb
Elluito <alfredoavendano@hotmail.com>
2024-08-02 14:02:13 +0100

    Test

--- slurm_handler.sh
+++ slurm_handler.sh
@@ -252,7 +252,8 @@ dataset=$2
 echo "model ${model} and dataset ${dataset}"
 
 #pruning_rates=("0.5" "0.6" "0.7" "0.8" "0.9" "0.95")
-pruning_rates=("0.95")
+pruning_rates=("0.7")
+
 # For resnet18
 
 if [ "${3}" -eq 1 ]; then
@@ -262,8 +263,8 @@ else
 
     rf_levels=("0.007" "0.008" "0.01")
 fi
-#pruners=("global" "lamp")
-pruners=("global")
+pruners=("global" "lamp")
+#pruners=("global")
 pruners_max=${#pruners[@]}                                  # Take the length of that array
 levels_max=${#rf_levels[@]}                                  # Take the length of that array
 number_pruning_rates=${#pruning_rates[@]}                            # Take the length of that array
@@ -292,7 +293,7 @@ done
 #run_soup_stochastic vgg19 cifar10 0
 #run_soup_stochastic vgg19 cifar100 0
 
-run_soup_stochastic resnet18 cifar10 1
+run_soup_stochastic resnet18 cifar10 0
 #run_soup_stochastic resnet18 cifar100 1
 
 
