10238ee52d46296da2794b5cdb259617db64db7d
Elluito <alfredoavendano@hotmail.com>
2024-08-14 22:17:49 +0100

    Results from the truncated models

--- slurm_truncated_handler.sh
+++ slurm_truncated_handler.sh
@@ -1,3 +1,37 @@
+#!/bin/bash
+# Submission script for serial Python job
+# Luis Alfredo Avendano  2021-10-22
+
+# Run from the current directory and with current environment
+#$ -V -cwd
+
+# Ask for some time (hh:mm:ss max of 00:10:00)
+#$ -l h_rt=07:00:00
+
+# ASk for some GPU
+# -l coproc_p100=1
+
+# Ask for some memory (by default, 1G, without a request)
+# -l node_type=40core-768G
+# -l h_vmem=600G
+#$ -l h_vmem=120G
+# -t 2-5
+# -pe smp 3
+# Send emails when job starts and ends
+#$ -m be
+#export OMP_NUM_THREADS=3
+#export MKL_NUM_THREADS=3
+#export OPENBLAS_NUM_THREADS=1
+# Now run the job
+#module load intel openmpi
+#module add anaconda
+#module add cuda/11.1.1
+
+#conda activate work
+#which python
+#unset GOMP_CPU_AFFINITY
+#unset KMP_AFFINITY
+
 
 CHECK_1="$HOME/checkpoints_temp"
 CHECK_2="$HOME/checkpoints_arc4_2/checkpoints"
