0f08d09fdfa022edee34c66d6c4ed7f348aa9c43
Elluito <alfredoavendano@hotmail.com>
2024-08-15 17:25:35 +0100

    Results from the truncated models

--- alternate_models/densenet.py
+++ alternate_models/densenet.py
@@ -103,5 +103,71 @@ def test():
     x = torch.randn(1,3,32,32)
     y = net(x)
     print(y)
+def get_features_only_until_block_layer_densenet_pytorch(net, block=2, net_type=0):
+    def features_only(self, x):
+       out = self.features(x)
+       return out
+    net.forward = features_only.__get__(net)  # bind method
+
+def get_features_only_until_block_layer_densenet(net, block=2, net_type=0):
+    # ResNet block to compute receptive field for
+
+    def features_only(self, x):
+        out = self.conv1(x)
+        if block == 0: return out
+        out = self.trans1(self.dense1(out))
+        if block == 1: return out
+        out = self.trans2(self.dense2(out))
+        if block == 2: return out
+        out = self.trans3(self.dense3(out))
+        if block == 3: return out
+        out = self.dense4(out)
+        if block == 4: return out
+        out = F.avg_pool2d(F.relu(self.bn(out)), 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    net.forward = features_only.__get__(net)  # bind method
+def test_rf():
+    from easy_receptive_fields_pytorch.receptivefield import receptivefield, give_effective_receptive_field
+    from main import get_features_only_until_block_layer
+    from torchvision.models import densenet121
+    pytorch_dense_net = densenet121()
+    net1= densenet_cifar()
+    net2= DenseNet121()
+
+    save_onnx(pytorch_dense_net,"pytorch_densenet_121")
+    save_onnx(net1,"densenet_cifar")
+    save_onnx(net2,"local_densenet_121")
+    blocks=[0,1,2,3,4]
+    print("RF of pytorch implementation")
+    get_features_only_until_block_layer_densenet_pytorch(pytorch_dense_net)
+    size = [1, 3, 224, 224]
+    le_rf = receptivefield(pytorch_dense_net, size)
+    print(le_rf.rfsize)
+    for block in blocks:
+        print("Block {}".format(block))
+        get_features_only_until_block_layer_densenet(net1, block=block, net_type=1)
+
+        size = [1, 3, 224, 224]
+
+        le_rf = receptivefield(net1, size)
+        print(le_rf.rfsize)
+
+
+def save_onnx(net, name: str = ""):
+    input_names = ['Image']
+
+    output_names = ['y_hat']
+
+    size = [2, 3, 32, 32]
+    dummy_input = torch.randn(size)
+    torch.onnx.export(net, dummy_input,
+                      f'/home/luisaam/PycharmProjects/sparse_ensemble/onnx_models/onnx_models_{name}.onnx',
+                      input_names=input_names,
+                      output_names=output_names)
+    # y = net(torch.randn(3, 3, 32, 32))
+if __name__ == '__main__':
+    test_rf()
 
 # test()
