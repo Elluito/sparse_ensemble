272bbb0dd60cc843bb684824c77137502343eb40
Elluito <alfredoavendano@hotmail.com>
2024-08-19 12:00:21 +0100

    truncated models resnet 50 cifar lvl 9 All levels vgg19 and tiny_imagenet, All levels resnet50 and tiny_imagenet.

--- slurm_truncated_handler.sh
+++ slurm_truncated_handler.sh
@@ -153,7 +153,6 @@ sol3vgg=/jmain02/home/J2AD014/mtc03/lla98-mtc03/original_paper_checkpoints/vgg19
 sol4vgg=/jmain02/home/J2AD014/mtc03/lla98-mtc03/original_paper_checkpoints/vgg19_normal_cifar10_seed.0_rf_level_4_recording_200_no_ffcv_test_acc_86.12.pth
 
 # Resnet50 CIFAR10
-
 sol1rs50=/jmain02/home/J2AD014/mtc03/lla98-mtc03/original_paper_checkpoints/resnet50_normal_cifar10_seed.0_rf_level_1_recording_200_no_ffcv_test_acc_94.99.pth
 sol2rs50=/jmain02/home/J2AD014/mtc03/lla98-mtc03/original_paper_checkpoints/resnet50_normal_cifar10_seed.0_rf_level_2_recording_200_no_ffcv_test_acc_94.24.pth
 sol3rs50=/jmain02/home/J2AD014/mtc03/lla98-mtc03/original_paper_checkpoints/resnet50_normal_cifar10_seed.0_rf_level_3_recording_200_no_ffcv_test_acc_92.3.pth
@@ -191,8 +190,8 @@ levels_max=${#rf_levels[@]}                                  # Take the length o
 #run_paper_truncated "resnet50" "cifar10" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
 #idxA=3
 #run_paper_truncated "resnet50" "cifar10" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
-idxA=4
-run_paper_truncated "resnet50" "cifar10" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
+#idxA=4
+#run_paper_truncated "resnet50" "cifar10" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
 
 
 
@@ -208,15 +207,18 @@ sol4vgg=/jmain02/home/J2AD014/mtc03/lla98-mtc03/original_paper_checkpoints/vgg19
 rf_levels=("1" "2" "3" "4")
 solutions=("${sol1vgg}" "${sol2vgg}" "${sol3vgg}" "${sol4vgg}")
 names=("seed_0" "seed_0" "seed_0" "seed_0")
-levels_max=${#rf_levels[@]}                                  # Take the length of that array
-for ((idxA=0; idxA<levels_max; idxA++));do              # iterate idxB from 0 to length
+#levels_max=${#rf_levels[@]}                                  # Take the length of that array
+#for ((idxA=0; idxA<levels_max; idxA++));do              # iterate idxB from 0 to length
+#
+#run_paper_truncated "vgg19" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
+#
+#done
+#
 
+idxA=2
 run_paper_truncated "vgg19" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
-
-done
-
-
-
+idxA=3
+run_paper_truncated "vgg19" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
 
 
 
@@ -239,10 +241,21 @@ solutions=("${sol2rs50}" "${sol3rs50}" "${sol4rs50}" "${sol5rs50}" "${sol6rs50}"
 
 names=("seed_0" "seed_0" "seed_0" "seed_0" "seed_0" "seed_0" "seed_0")
 
-levels_max=${#rf_levels[@]}                                  # Take the length of that array
+#levels_max=${#rf_levels[@]}                                  # Take the length of that array
+#
+#for ((idxA=0; idxA<levels_max; idxA++));do              # iterate idxB from 0 to length
+#
+#run_paper_truncated "resnet50" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
+#
+#done
 
-for ((idxA=0; idxA<levels_max; idxA++));do              # iterate idxB from 0 to length
 
+
+idxA=1
 run_paper_truncated "resnet50" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
-
-done
+idxA=2
+run_paper_truncated "resnet50" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
+idxA=3
+run_paper_truncated "resnet50" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
+idxA=4
+run_paper_truncated "resnet50" "tiny_imagenet" "${rf_levels[$idxA]}" "${names[$idxA]}" "${solutions[$idxA]}" "${save_folder}" "50" "0.001" "0"
