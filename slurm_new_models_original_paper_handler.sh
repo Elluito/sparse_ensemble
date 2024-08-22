e384448dc6050617725aa75988addb7bc3761780
Elluito <alfredoavendano@hotmail.com>
2024-08-22 20:57:59 +0100

    truncated models resnet 50 cifar lvl 9 All levels vgg19 and tiny_imagenet, All levels resnet50 and tiny_imagenet.

--- slurm_new_models_original_paper_handler.sh
+++ slurm_new_models_original_paper_handler.sh
@@ -2,7 +2,7 @@
 for lvl in 1 2 3 4 9 10 11; do                # iterate idxa from 0 to length
 for model in "denset40" "mobilenetv2";do
 for dataset in "cifar10" "tiny_imagenet"; do
-sbatch --nodes=1 --time=96:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_deep_small_resnet.sh "${model}" "${dataset}" 4 ${lvl}  "normal" 200 "recording_200" 1 1
+sbatch --nodes=1 --time=96:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 4 ${lvl}  "normal" 200 "recording_200" 1 1
 done
 done
 done
