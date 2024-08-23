0eb16d92bb8d6d808b60650ee2948fbac354969a
Elluito <alfredoavendano@hotmail.com>
2024-08-23 16:11:05 +0100

    truncated models resnet 50 cifar lvl 9 All levels vgg19 and tiny_imagenet, All levels resnet50 and tiny_imagenet.

--- slurm_new_models_original_paper_handler.sh
+++ slurm_new_models_original_paper_handler.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 for lvl in 1 2 4 8; do                # iterate idxa from 0 to length
-for model in "denset40" "mobilenetv2";do
+for model in "densenet40";do
 for dataset in "cifar10" "tiny_imagenet"; do
-sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 4 ${lvl}  "normal" 200 "recording_200" 1 1
+sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 2 ${lvl}  "normal" 200 "recording_200" 1 1
+done
+done
+done
+
+for lvl in 2 4 8; do                # iterate idxa from 0 to length
+for model in  "mobilenetv2";do
+for dataset in "cifar10"; do
+sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 2 ${lvl}  "normal" 200 "recording_200" 1 1
+done
+done
+done
+for lvl in 1 4; do                # iterate idxa from 0 to length
+for model in  "mobilenetv2";do
+for dataset in "tiny_imagenet"; do
+sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 2 ${lvl}  "normal" 200 "recording_200" 1 1
 done
 done
 done
--- slurm_new_models_original_paper_run.sh
+++ slurm_new_models_original_paper_run.sh
@@ -25,6 +25,7 @@
 
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=4
+#SBATCH --mem-per-cpu=2GB
 
 # send mail to this address
 #SBATCH --mail-user=sclaam@leeds.ac.uk
