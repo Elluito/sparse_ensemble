93d5d123e5c4c1bd9a494a95b503db9e2e234757
Elluito <alfredoavendano@hotmail.com>
2024-09-02 11:10:55 +0100

    Extraction of features by models

--- slurm_probes_extraction_run.sh
+++ slurm_probes_extraction_run.sh
@@ -6,4 +6,4 @@ export LD_LIBRARY_PATH=""
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib"
 export PYTHONPATH="/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib/python3.9/site-packages"
 
-python3.9 extractor_external.py -f $1 -m $2 -t $3 -s $4 -d $5 --RF_level $6 --input_resolution $7 --num_workers $8  --batch_size $9 --save_path "${10}"
+python3.9 extraction_external.py -f $1 -m $2 -t $3 -s $4 -d $5 --RF_level $6 --input_resolution $7 --num_workers $8  --batch_size $9 --save_path "${10}"
