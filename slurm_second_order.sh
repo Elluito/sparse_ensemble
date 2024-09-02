7509da566cb2359f0a900999a10c420296422258
Elluito <alfredoavendano@hotmail.com>
2024-09-02 11:34:37 +0100

    Extraction of features by models

--- slurm_second_order.sh
+++ slurm_second_order.sh
@@ -36,16 +36,16 @@ which python
 #
 #python Second_order_Receptive_field.py --experiment 2 --optimiser "sam"
 
-#  if [ $6 -eq 1 ]
-#  then
-#    # KFac
-#python Second_order_Receptive_field.py --lr "0.01" --momentum "0.5" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 32 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "kfac" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
-#
-#  fi
-#
-#  if [ $6 -eq 2 ]
-#    # SAM
-#  then
-#python Second_order_Receptive_field.py --lr "0.1" --momentum "0.7" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 128 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "sam" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
-#  fi
-#
+  if [ $6 -eq 1 ]
+  then
+    # KFac
+python Second_order_Receptive_field.py --lr "0.01" --momentum "0.5" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 32 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "kfac" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+
+  fi
+
+  if [ $6 -eq 2 ]
+    # SAM
+  then
+python Second_order_Receptive_field.py --lr "0.1" --momentum "0.7" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 128 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "sam" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+  fi
+
