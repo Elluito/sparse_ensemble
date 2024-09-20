a0d1ba0d110b1a08ca24e83928e8be3251f58c94
Elluito <alfredoavendano@hotmail.com>
2024-09-20 16:49:47 +0100

    Second order optimisers

--- train_probes.py
+++ train_probes.py
@@ -32,10 +32,10 @@ if __name__ == '__main__':
 
     parser.add_argument('-m', dest='model', type=str, default="vgg19", help='Model architecture')
     parser.add_argument('-d', dest='dataset', type=str, default="cifar10", help='Dataset to use')
-    parser.add_argument('-n', dest='name', type=str, default="", help='Name of the run')
     parser.add_argument('--RF_level', dest='RF_level', type=int, default=2, help='Receptive field level')
-    parser.add_argument('--input_resolution', dest='input_resolution', type=int, default=32, help='Input resolution')
     parser.add_argument('-mp', dest='mp', type=int, default=cpu_count(), help='Enable multiprocessing')
+    parser.add_argument('--name', dest='name', type=str, default="", help='Name of the run')
+    parser.add_argument('--input_resolution', dest='input_resolution', type=int, default=32, help='Input resolution')
     parser.add_argument('--save_path', default="./probes_logs/", type=str, help='Save path of logs')
     parser.add_argument('--prefix', dest='prefix', type=str, default=None, help='Postfix added to the result csv')
     parser.add_argument('-f', dest='folder', type=str, default="./latent_datasets", help='data folder')
