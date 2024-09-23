9d6cb1e3537ef5f818ee0c5badbfe6e7da83c512
Elluito <alfredoavendano@hotmail.com>
2024-09-23 18:04:04 +0100

    Second order optimisers

--- slurm_2nd_order_handler.sh
+++ slurm_2nd_order_handler.sh
@@ -40,14 +40,14 @@ grad_clip=0
 grad_clip=0
 
 
-sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_1_cifar10_rs.err" --output="sam_optim_rf_1_cifar10_rs.output" --job-name="sam_optim_rf_1_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "1" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
-sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_2_cifar10_rs.err" --output="sam_optim_rf_2_cifar10_rs.output" --job-name="sam_optim_rf_2_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "2" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
+#sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_1_cifar10_rs.err" --output="sam_optim_rf_1_cifar10_rs.output" --job-name="sam_optim_rf_1_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "1" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
+#sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_2_cifar10_rs.err" --output="sam_optim_rf_2_cifar10_rs.output" --job-name="sam_optim_rf_2_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "2" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
 
 
 
 
-sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_3_cifar10_rs.err" --output="sam_optim_rf_3_cifar10_rs.output" --job-name="sam_optim_rf_3_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "3" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
-sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_4_cifar10_rs.err" --output="sam_optim_rf_4_cifar10_rs.output" --job-name="sam_optim_rf_4_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "4" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
+sbatch --nodes=1 --array=1-2 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_3_cifar10_rs.err" --output="sam_optim_rf_3_cifar10_rs.output" --job-name="sam_optim_rf_3_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "3" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
+#sbatch --nodes=1 --array=1-3 --time=50:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="sam_optim_rf_4_cifar10_rs.err" --output="sam_optim_rf_4_cifar10_rs.output" --job-name="sam_optim_rf_4_cifar10_rs" slurm_2nd_order_run.sh "cifar10" "resnet50" "4" "normal" "sam_optim_saturation_200_gc_${grad_clip}" "2" "${grad_clip}" 1
 
 
 #
