9ea5d04a1dbd57637113475c8588ae80308411b1
Elluito <alfredoavendano@hotmail.com>
2024-09-30 15:19:18 +0100

    Second order optimisers

--- slurm_pruning_handler.sh
+++ slurm_pruning_handler.sh
@@ -78,7 +78,7 @@ else
 for model in "resnet50"; do
 for dataset in "cifar10"; do
 for pruning_rate in "0.8"; do
-for rf_level in "1" "2" "3" "4"; do
+for rf_level in "3" "4"; do
 
 run_pruning "${model}" "$dataset" "${HOME}/second_order_saturation" "${HOME}/datasets" "${HOME}/sparse_ensemble/second_order_pruning" "ekfac_optim_hyper_saturation_200_gc_0" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}"
 
