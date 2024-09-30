626ec5e7372416ee3f518b5abe7ee36b5676275a
Elluito <alfredoavendano@hotmail.com>
2024-09-30 11:34:33 +0100

    Second order optimisers

--- slurm_inter_layer_handler.sh
+++ slurm_inter_layer_handler.sh
@@ -57,7 +57,7 @@ else
 for model in "vgg19"; do
 for dataset in "cifar10"; do
 for pruning_rate in "0.9"; do
-for rf_level in "1" "2" "3"; do
+for rf_level in "1" "2" "3" "4"; do
 
 run_pruning "${model}" "$dataset" "${HOME}/original_paper_checkpoints" "${HOME}/datasets" "${HOME}/sparse_ensemble/inter_layer_pruning_results" "recording_200" 4 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}"
 
