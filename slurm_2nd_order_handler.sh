c023b818f3ec89e8b58f09607ce4a18b0f8800c5
Elluito <alfredoavendano@hotmail.com>
2024-10-06 10:20:26 +0100

    Second order optimisers

--- slurm_2nd_order_handler.sh
+++ slurm_2nd_order_handler.sh
@@ -18,20 +18,19 @@ grad_clip=0
 
 #sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_1_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_1_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_1_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "1" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
 #sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_2_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_2_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_2_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "2" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
-#
-#
-#
+sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "3" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 0
+#sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}.output" --job-name="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "4" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
+
 sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_5_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_5_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_5_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "5" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
 sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_6_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_6_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_6_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "6" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
 sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_7_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_7_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_7_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "7" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
-#sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}.output" --job-name="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "4" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
-#
-#
-#
+
+
+
 #sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_1_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_1_cifar10_vgg19_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_1_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "1" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
 #sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_2_cifar10_vgg19_50_gc_${grad_clip}.err" --output="ekfac_optim_rf_2_cifar10_vgg19_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_2_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "2" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
-#
-#
+
+
 #sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_3_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_3_cifar10_vgg19_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_3_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "3" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
 #sbatch --nodes=1 --array=1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_4_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_4_cifar10_vgg19_gc_${grad_clip}.output" --job-name="ekfac_optim_rf_4_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "4" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
 
