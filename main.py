469135ecd1bd1e2965ba56d94912ca6bb518baa2
Elluito <alfredoavendano@hotmail.com>
2024-10-14 16:44:31 +0100

    Second order optimisers

--- main.py
+++ main.py
@@ -2468,43 +2468,6 @@ def get_cifar_datasets(cfg: omegaconf.DictConfig):
         testloader = torch.utils.data.DataLoader(
             testset, batch_size=100, shuffle=False, num_workers=cfg.num_workers)
 
-    if cfwg.dataset == "cifar100":
-
-        trainset = torchvision.datasets.CIFAR100(
-            root=data_path, train=True, download=True, transform=transform_train)
-        trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=128, shuffle=True, num_workers=cfg.num_workers)
-
-        testset = torchvision.datasets.CIFAR100(
-            root=data_path, train=False, download=True, transform=transform_test)
-        testloader = torch.utils.data.DataLoader(
-            testset, batch_size=100, shuffle=False, num_workers=cfg.num_workers)
-        transform_train = transforms.Compose([
-            transforms.RandomCrop(32, padding=4),
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-
-        transform_test = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-        ])
-
-
-        trainset = torchvision.datasets.CIFAR10(root=data_path, train=True, download=True, transform=transform_train)
-
-        cifar10_train, cifar10_val = random_split(trainset, [len(trainset) - 5000, 5000])
-
-        trainloader = torch.utils.data.DataLoader(cifar10_train, batch_size=cfg.batch_size, shuffle=True,
-                                                  num_workers=cfg.num_workers)
-        val_loader = torch.utils.data.DataLoader(cifar10_val, batch_size=cfg.batch_size, shuffle=True,
-                                                 num_workers=cfg.num_workers)
-        testset = torchvision.datasets.CIFAR10(root=data_path, train=False, download=False, transform=transform_test)
-        testloader = torch.utils.data.DataLoader(testset, batch_size=cfg.batch_size, shuffle=False,
-                                                 num_workers=cfg.num_workers)
-        return trainloader, val_loader, testloader
-
     if cfg.dataset == "cifar100":
         current_directory = Path().cwd()
         data_path = ""
