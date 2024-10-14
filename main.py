fe1a985ffb67c52dc635a7bd707cb67982c97261
Elluito <alfredoavendano@hotmail.com>
2024-10-14 17:46:04 +0100

    Second order optimisers

--- main.py
+++ main.py
@@ -2457,17 +2457,20 @@ def get_cifar_datasets(cfg: omegaconf.DictConfig):
                                                  transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
                                                  ])
 
+        cifar10_train, cifar10_val = random_split(trainset, [45000, 5000])
 
         trainset = torchvision.datasets.CIFAR10(
             root=data_path, train=True, download=True, transform=transform_train)
         trainloader = torch.utils.data.DataLoader(
-            trainset, batch_size=128, shuffle=True, num_workers=cfg.num_workers)
+            cifar10_train, batch_size=cfg.batch_size, shuffle=True, num_workers=cfg.num_workers)
+        val_loader = torch.utils.data.DataLoader(cifar10_val, batch_size=cfg.batch_size, shuffle=True,
+                                                 num_workers=cfg.num_workers)
 
         testset = torchvision.datasets.CIFAR10(
             root=data_path, train=False, download=True, transform=transform_test)
         testloader = torch.utils.data.DataLoader(
             testset, batch_size=100, shuffle=False, num_workers=cfg.num_workers)
-
+        return trainloader,val
     if cfg.dataset == "cifar100":
         current_directory = Path().cwd()
         data_path = ""
