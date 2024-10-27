d0168b83705a4ed3f382a2a1cf0fc70058b69db3
Elluito <alfredoavendano@hotmail.com>
2024-10-27 22:20:09 +0000

    Second order optimisers

--- slurm_pruning_handler.sh
+++ slurm_pruning_handler.sh
@@ -102,7 +102,7 @@ else
 resolution=224
 
 for model in "resnet25_small"; do
-for dataset in "small_resnet"; do
+for dataset in "small_imagenet"; do
 for pruning_rate in "0.8"; do
 for rf_level in "5" "6" "7" "8" "10"; do
 
