21c10f4627ecb80e13d39fade1d5c12a790ee8bb
Elluito <alfredoavendano@hotmail.com>
2024-10-28 16:33:24 +0000

    Second order optimisers

--- slurm_weigth_statistics_handler.sh
+++ slurm_weigth_statistics_handler.sh
@@ -125,9 +125,9 @@ for pruning_rate in "0"; do
 for rf_level in "1" "2" "3" "4"; do
 
 
-run_statistics "${model}" "$dataset" "${HOME}/original_paper_checkpoints" "${HOME}/datasets" "${HOME}/sparse_ensemble/weight_distributions" "recording_200" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}" "${resolution}"
-run_statistics "${model}" "$dataset" "${HOME}/second_order_saturation" "${HOME}/datasets" "${HOME}/sparse_ensemble/weight_distributions" "sam_optim_saturation_200_gc_0" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}" "${resolution}"
-run_statistics "${model}" "$dataset" "${HOME}/second_order_saturation" "${HOME}/datasets" "${HOME}/sparse_ensemble/weight_distributions" "ekfac_optim_hyper_saturation_200_gc_0" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}" "${resolution}"
+run_statistics "${model}" "$dataset" "${HOME}/deep_small_models_saturation" "${HOME}/datasets" "${HOME}/sparse_ensemble/weight_distributions" "sgd_200_res_32_lr_0.01" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}" "${resolution}"
+run_statistics "${model}" "$dataset" "${HOME}/deep_small_models_saturation" "${HOME}/datasets" "${HOME}/sparse_ensemble/weight_distributions" "lr_changed_sam_200_res_32_gc_0" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}" "${resolution}"
+run_statistics "${model}" "$dataset" "${HOME}/deep_small_models_saturation" "${HOME}/datasets" "${HOME}/sparse_ensemble/weight_distributions" "lr_changed_ekfac_200_res_32_gc_0" 0 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv" "${pruning_rate}" "${rf_level}" "${resolution}"
 
 
 done
