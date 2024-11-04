103021374e2e15462db37816fb6aaf360210e682
Elluito <alfredoavendano@hotmail.com>
2024-11-04 14:31:59 +0000

    Second order optimisers

---
+++ arc4_training_handler.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_1_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_1_cifar10_vgg19_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_1_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "1" "normal" "ekfac_optim_hyper_100_gc_${grad_clip}" "1" grad_clip
+#sbatch --nodes=1 --time=24:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_2_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_2_cifar10_vgg19_gc_${grad_clip}.output" --job-name="ekfac_optim_rf_2_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "2" "normal" "ekfac_optim_hyper_100_gc_${grad_clip}" "1" grad_clip
+#sbatch --nodes=1 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_3_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_3_cifar10_vgg19_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_3_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "3" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" grad_clip
+#sbatch --nodes=1 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_4_cifar10_vgg19_gc_${grad_clip}.err" --output="ekfac_optim_rf_4_cifar10_vgg19_gc_${grad_clip}.output" --job-name="ekfac_optim_rf_4_cifar10_vgg19_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "vgg19" "4" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" grad_clip
+run_2nd_order_experiment(){
+model=$1
+dataset=$2
+algorithm=$3
+name=$4
+epochs=$5
+grad_clip=$6
+RF_level=$7
+record_saturation=$8
+array=$9
+input_res="${10}"
+
+if [ "${array}" -eq 1 ]; then
+
+#sbatch --nodes=1 --array=1-4 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}.err" --output="${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}.out"  --job-name="${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}" slurm_2nd_order_run.sh "${dataset}" "${model}" "${RF_level}" "normal" "${name}" "${algorithm}" "${grad_clip}" "${record_saturation}" "${epochs}" "${input_res}"
+
+qsub -l h_rt=44:00:00 -t 1-3 -l coproc_v100=1  -N  "${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}" arc4_2nd_order_run.sh "${dataset}" "${model}" "${RF_level}" "normal" "${name}" "${algorithm}" "${grad_clip}" "${record_saturation}" "${epochs}" "${input_res}"
+
+else
+
+#sbatch --nodes=1 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}.err" --output="${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}.out"  --job-name="${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}" slurm_2nd_order_run.sh "${dataset}" "${model}" "${RF_level}" "normal" "${name}" "${algorithm}" "${grad_clip}" "${record_saturation}" "${epochs}" "${input_res}"
+qsub -l h_rt=36:00:00 -l coproc_v100=1  -N  "${algorithm}_rf_${RF_level}_${dataset}_${model}_gc_${grad_clip}" arc4_2nd_order_run.sh "${dataset}" "${model}" "${RF_level}" "normal" "${name}" "${algorithm}" "${grad_clip}" "${record_saturation}" "${epochs}" "${input_res}"
+fi
+}
+
+#grad_clip=1
+#
+#sbatch --nodes=1 --array 1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "3" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" grad_clip
+#sbatch --nodes=1 --array 1-3 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}.output" --job-name="ekfac_optim_rf_4_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "4" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" grad_clip
+
+#sbatch --nodes=1 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_1_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_1_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "1" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
+#sbatch --nodes=1 --time=140:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk --error="ekfac_optim_rf_2_cifar10_resnet50_gc_${grad_clip}.err" --output="ekfac_optim_rf_2_cifar10_resnet50_gc_${grad_clip}.output"  --job-name="ekfac_optim_rf_3_cifar10_resnet50_gc_${grad_clip}" slurm_2nd_order_run.sh "cifar10" "resnet50" "2" "normal" "ekfac_optim_hyper_saturation_200_gc_${grad_clip}" "1" "${grad_clip}" 1
+
+res=64
+epochs=200
+array=1
+for model in "resnet25_small"; do
+for dataset in "small_imagenet"; do
+#for model in "deep_small_vgg" "resnet25_small"; do # all two models
+#for lvl in 1 2 3 4; do                #
+for lvl in 5 6 7 8 10; do                # iterate idxA from 0 to length
+#for lvl in 5; do                # iterate idxA from 0 to length
+if [ "${array}" -eq 1 ]; then
+
+qsub -l h_rt=44:00:00 -t 1-3 -l coproc_v100=1 -N "deep_${model}_lvl_${lvl}_${dataset}_res_${res}" arc4_training_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}_lr_0.01" 1 0 "${res}"
+
+else
+
+ qsub -l h_rt=36:00:00 -l coproc_v100=1 "deep_${model}_lvl_${lvl}_${dataset}_res_${res}"  arc4_training_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}_lr_0.01" 1 0 "${res}"
+fi
+
+#sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="deep_{}_lvl_${lvl}_small_imagenet_res_${res}.err" --gres=gpu:1 --output="deep_small_resnet_lvl_${lvl}_small_imagenet_res_${res}.output"  --job-name="deep_small_resnet_lvl_${lvl}_small_imagenet_res_${res}" slurm_deep_small_resnet_run.sh "resnet25_small" "small_imagenet" 8 ${lvl}  "normal" "${epochs}" "recording_${epochs}" 1 1 "${res}"
+#sbatch --nodes=1 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="resnet_small_lvl_${lvl}_small_imagenet_res_${res}_saturation_${epochs}.err" --gres=gpu:1 --output="resnet_small_lvl_${lvl}_small_imagenet_res_${res}_saturation_${epochs}.output"  --job-name="resnet_small_lvl_${lvl}_small_imagenet_res_${res}_saturation_${epochs}" slurm_deep_small_resnet_run.sh "resnet_small" "small_imagenet" 4 ${lvl}  "normal" 100 "recording_100_res_${res}_ffcv" 1 1 "${res}" 1 "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv" "/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv"
+done
+done
+done
+
---
+++ arc4_training_run.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+# Submission script for serial Python job
+# Luis Alfredo Avendano  2021-10-22
+
+# Run from the current directory and with current environment
+#$ -V -cwd
+
+# Ask for some time (hh:mm:ss max of 00:10:00)
+#$ -l h_rt=07:00:00
+
+# ASk for some GPU
+# -l coproc_p100=1
+
+# Ask for some memory (by default, 1G, without a request)
+# -l node_type=40core-768G
+# -l h_vmem=600G
+#$ -l h_vmem=8G
+# -t 2-5
+# -pe smp 3
+# Send emails when job starts and ends
+#$ -m be
+
+
+#for ARGUMENT in "$@"
+#do
+#   KEY=$(echo $ARGUMENT | cut -f1 -d=)
+#
+#   KEY_LENGTH=${#KEY}
+#   VALUE="${ARGUMENT:$KEY_LENGTH+1}"
+#
+#   export "$KEY"="$VALUE"
+#done
+#
+## use here your expected variables
+#echo "FFCV = $FFCV"
+#echo "NAME = $NAME"
+#
+#echo "MODEL = $MODEL"
+#echo "DATASET= $DATASET"
+#echo "NUMW= $NUMW"
+#echo "RFL=$RFL"
+#echo "TYPE= $TYPE"
+#echo "FOLDER= $FOLDER"
+#echo "PR= $PR"
+#echo "EXPERIMENT= $EXPERIMENT"
+#echo "FFCV_TRAIN = $FFCV_TRAIN "
+#echo "FFCV_VAL = $FFCV_VAL "
+#
+##    python prune_models.py --name "${NAME}" --ffcv --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --folder $6 --pruning_rate $7 --experiment $8
+#
+#echo "EXTRA_VALUES = $EXTRA_VALUES"
+
+
+#python train_CIFAR10.py --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5
+
+#python Second_order_Receptive_field.py --experiment 2 --optimiser "kfac"
+#
+#python Second_order_Receptive_field.py --experiment 2 --optimiser "sam"
+#export LD_LIBRARY_PATH=""
+#l=$(which python)
+#lib_path_of_current_enviroment="${l%%python}"
+#echo "Ld library ${lib_path_of_current_enviroment}"
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$lib_path_of_current_enviroment
+#export PYTHONPATH="/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib/python3.9/site-packages"
+
+conda activate work
+which python
+unset GOMP_CPU_AFFINITY
+unset KMP_AFFINITY
+
+if [ "${11}" -gt 0 ]
+  then
+  echo "Use FFCV"
+#sbatch --nodes=1 --time=07:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk  --error="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_confidence_ffcv.err" --output="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_confidence_ffcv.out" --job-name="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_confidence_ffcv" slurm_confidence_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}" DATA_FOLDER="${data_folder}" OUTPUT_DIR="${output_dir}" TOPK=10
+
+python3.9 train_CIFAR10.py --ffcv --lr "0.1" --batch_size 128  --save_folder "/nobackup/sclaam/training_models_arc4/" --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --epochs $6  --name $7 --width $8 --record $9 --input_resolution "${10}"  --ffcv_val "${12}" --ffcv_train "${13}" --record_saturation 0
+
+#python3.9 ffcv_loaders.py
+
+# slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}"
+
+#./slurm_pruning_run.sh FFCV=0 NAME=recording_200_no_ffcv MODEL=resnet_small DATASET=small_imagenet NUMW=4 RFL=10 TYPE=normal FOLDER=$HOME/checkpoints PR=0.6 EXPERIMENT=1
+else
+  echo "Don't use FFCV"
+
+python3.9 train_CIFAR10.py --lr "0.1" --batch_size 128  --save_folder "/nobackup/sclaam/training_models_arc4/" --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --epochs $6  --name $7 --width $8 --record $9 --input_resolution "${10}" --record_saturation 0
+#python3.9 ffcv_loaders.py
+
+  fi
+
+#python3.9 Second_order_Receptive_field.py --level $1
+#python zero_cost_nas_RF.py
