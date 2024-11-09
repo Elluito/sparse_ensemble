b8cbe16f00c045a8245107e3a529bf64821e4295
Elluito <alfredoavendano@hotmail.com>
2024-11-09 21:13:31 +0000

    Second order optimisers

--- slurm_deep_small_resnet_handler.sh
+++ slurm_deep_small_resnet_handler.sh
@@ -28,9 +28,9 @@
 #done
 #done
 
-res=426
-epochs=10
-array=0
+res=224
+epochs=100
+array=1
 ffcv=0
 ffcv_train="/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv"
 ffcv_test="/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv"
@@ -48,13 +48,13 @@ for dataset in "small_imagenet"; do
 #for dataset in "cifar10"; do
 #for model in "deep_small_vgg" "resnet25_small"; do # all two models
 #for lvl in 13; do                # iterate idxA from 0 to length
-for lvl in 5;do
+for lvl in 5 6 7 8 10;do
 #for lvl in 2 3; do                #
 #for lvl in 5; do                iterate idxA from 0 to length
-jname="deep_${model}_lvl_${lvl}_${dataset}_res_${res}_${ffcv_string}_large_image"
+jname="deep_${model}_lvl_${lvl}_${dataset}_res_${res}_${ffcv_string}_more_models"
 if [ "${array}" -eq 1 ]; then
 
-sbatch --ntasks-per-node=1 --cpus-per-task=16 --nodes=1 --array=1-2 --time=144:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${jname}.err" --gres=gpu:1 --output="${jname}.out"  --job-name="${jname}" slurm_deep_small_resnet_run.sh "${model}" "${dataset}" 16 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}_${ffcv_string}" 1 0 "${res}" "${ffcv}" "${ffcv_train}" "${ffcv_test}" "${save_folder}" "${batch_size}"
+sbatch --ntasks-per-node=1 --cpus-per-task=16 --nodes=1 --array=1-3 --time=144:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${jname}.err" --gres=gpu:1 --output="${jname}.out"  --job-name="${jname}" slurm_deep_small_resnet_run.sh "${model}" "${dataset}" 16 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}_${ffcv_string}" 1 0 "${res}" "${ffcv}" "${ffcv_train}" "${ffcv_test}" "${save_folder}" "${batch_size}"
 
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
