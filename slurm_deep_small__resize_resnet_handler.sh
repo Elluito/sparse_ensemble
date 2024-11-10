54bf6173f0d135916b3184fb91471c6c1ac67692
Elluito <alfredoavendano@hotmail.com>
2024-11-10 22:18:59 +0000

    Second order optimisers

---
+++ slurm_deep_small__resize_resnet_handler.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+
+#res=224
+#epochs=100
+#array=1
+#ffcv=0
+#for model in "resnet25_small"; do
+#for dataset in "small_imagenet"; do
+##for model in "deep_small_vgg" "resnet25_small"; do # all two models
+#for lvl in 11 12 13; do                # iterate idxA from 0 to length
+##for lvl in 1 2 3 4; do                #
+##for lvl in 5; do                # iterate idxA from 0 to length
+#if [ "${array}" -eq 1 ]; then
+#
+#sbatch --ntasks-per-node=1 --cpus-per-task=16 --nodes=1 --array=1-3 --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="deep_${model}_lvl_${lvl}_${dataset}_res_${res}.err" --gres=gpu:1 --output="deep_${model}_lvl_${lvl}_${dataset}_res_${res}.output"  --job-name="deep_${model}_lvl_${lvl}_${dataset}_res_${res}" slurm_deep_small_resnet_run.sh "${model}" "${dataset}" 16 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}" 1 0 "${res}" "${ffcv}"
+#
+##SBATCH --ntasks-per-node=1
+##SBATCH --cpus-per-task=8
+#
+#else
+#
+#sbatch --ntasks-per-node=1 --cpus-per-task=16 --nodes=1  --time=140:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="deep_${model}_lvl_${lvl}_${dataset}_res_${res}.err" --gres=gpu:1 --output="deep_${model}_lvl_${lvl}_${dataset}_res_${res}.output"  --job-name="deep_${model}_lvl_${lvl}_${dataset}_res_${res}" slurm_deep_small_resnet_run.sh "${model}" "${dataset}" 16 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}" 1 0 "${res}" "${ffcv}"
+#
+#fi
+#
+#
+#done
+#done
+#done
+
+res=224
+epochs=100
+array=1
+ffcv=0
+ffcv_train="/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/train_360_0.5_90.ffcv"
+ffcv_test="/jmain02/home/J2AD014/mtc03/lla98-mtc03/small_imagenet_ffcv/val_360_0.5_90.ffcv"
+#save_folder="/jmain02/home/J2AD014/mtc03/lla98-mtc03/deep_small_models_ffcv"
+save_folder="/jmain02/home/J2AD014/mtc03/lla98-mtc03/deep_small_models_2"
+batch_size=128
+
+if [ "${ffcv}" -eq 1 ]; then
+  ffcv_string="ffcv"
+  else
+  ffcv_string="no_ffcv"
+  fi
+#for res in 1280 640 426; do
+for model in "resnet25_small"; do
+for dataset in "small_imagenet"; do
+#for dataset in "cifar10"; do
+#for model in "deep_small_vgg" "resnet25_small"; do # all two models
+#for lvl in 13; do                # iterate idxA from 0 to length
+for lvl in 5 6 7 8 10 11 12 13;do
+#for lvl in 2 3; do                #
+#for lvl in 5; do                iterate idxA from 0 to length
+jname="deep_${model}_lvl_${lvl}_${dataset}_res_${res}_${ffcv_string}_resized"
+if [ "${array}" -eq 1 ]; then
+
+sbatch --ntasks-per-node=1 --cpus-per-task=16 --nodes=1 --array=1-3 --time=144:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${jname}.err" --gres=gpu:1 --output="${jname}.out"  --job-name="${jname}" slurm_deep_small_resnet_run.sh "${model}" "${dataset}" 16 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}_${ffcv_string}" 1 0 "${res}" "${ffcv}" "${ffcv_train}" "${ffcv_test}" "${save_folder}" "${batch_size}"
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+
+else
+
+sbatch --ntasks-per-node=1 --cpus-per-task=16 --nodes=1  --time=144:00:00 --partition=small  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${jname}.err" --gres=gpu:1 --output="${jname}.out"  --job-name="${jname}" slurm_deep_small_resnet_run.sh "${model}" "${dataset}" 16 ${lvl}  "normal" "${epochs}" "sgd_${epochs}_res_${res}_${ffcv_string}" 1 0 "${res}" "${ffcv}" "${ffcv_train}" "${ffcv_test}" "${save_folder}" "${batch_size}"
+
+
+fi
+
+#done
+done
+done
+done
---
+++ slurm_deep_small_resize_run.sh
@@ -0,0 +1,127 @@
+#!/bin/bash
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set max wallclock time
+#SBATCH --time=00:09:00
+
+# set name of job
+#SBATCH --job-name=pytorch_test
+
+#SBATCH --error=pytorch_test.err
+
+#SBATCH --output=pytorch_test.output
+
+# set partition (devel, small, big)
+
+#SBATCH --partition=small
+
+# set number of GPUs
+#SBATCH --gres=gpu:1
+
+# mail alert at start, end and abortion of execution
+#SBATCH --mail-type=ALL
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=8
+
+# send mail to this address
+#SBATCH --mail-user=sclaam@leeds.ac.uk
+#module load pytorch
+
+
+#python train_CIFAR10.py --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5
+
+#python Second_order_Receptive_field.py --experiment 2 --optimiser "kfac"
+#
+#python Second_order_Receptive_field.py --experiment 2 --optimiser "sam"
+
+#  if [ $6 -eq 1 ]
+#  then
+#    # KFac
+#python Second_order_Receptive_field.py --lr "0.01" --momentum "0.5" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 32 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "kfac" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+#
+#  fi
+#
+#  if [ $6 -eq 2 ]
+#    # ASAM
+#  then
+#python Second_order_Receptive_field.py --lr "0.1" --momentum "0.7" --grad_clip "1" --save 1 --experiment 1 --epochs 100 --batch_size 128 --use_scheduler 1 --use_scheduler_batch 0 --num_workers 4 --optimiser "sam" --record 1 -dt $1 --model $2 --RF_level $3 --type $4 --name $5 --save_folder "$HOME/checkpoints"
+#  fi
+#
+#python -c "import torch;device = 'cuda' if torch.cuda.is_available() else 'cpu';print(device);print('Cuda version with torch: {}'.format(torch.version.cuda))"
+#python -c "import os; print(os.environ)"
+#printf "Start Test \n"
+#python test_backwards.py
+#printf "End Test \n"
+#module load pytorch
+#echo "After loading the pytorch module"
+#which python
+#python -c "import torch;device = 'cuda' if torch.cuda.is_available() else 'cpu';print(device);print('Cuda version with torch: {}'.format(torch.version.cuda))"
+#python -c "import os; print(os.environ)"
+#printf "Start Test \n"
+#python test_backwards.py
+#printf "End Test \n"
+##echo "============ 2 workers ============================"
+##python hao_models_pruning_test.py --workers 2
+#echo "============ 4 workers ============================"
+#python hao_models_pruning_test.py --workers 4
+#echo "============ 8 workers ============================"
+#python hao_models_pruning_test.py --workers 2 --experiment 2 --model $1
+#echo "============ 16 workers ============================"
+#python hao_models_pruning_test.py --workers 16
+#echo "============ 32 workers ============================"
+#python hao_models_pruning_test.py --workers 32
+
+#export LD_LIBRARY_PATH=""
+#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib"
+
+
+#unset GOMP_CPU_AFFINITY
+#unset KMP_AFFINITY
+
+module load python/miniconda
+conda activate ffcv2
+
+python -c "import os; print(os.environ)"
+#python -c "import torch;device = 'cuda' if torch.cuda.is_available() else 'cpu';print(device);print('Cuda version with torch: {}'.format(torch.version.cuda))"
+l=$(which python)
+
+#lib_path_of_current_enviroment="${l%%"bin/python"}"
+#echo "Ld library ${lib_path_of_current_enviroment}"
+#export LD_LIBRARY_PATH="${lib_path_of_current_enviroment}/lib":$LD_LIBRARY_PATH
+
+export LD_LIBRARY_PATH=""
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv2/lib"
+export PYTHONPATH="${lib_path_of_current_enviroment}/lib/python3.9/site-packages":PYTHONPATH
+#############################################################
+#     Train  without FFCV
+#############################################################
+
+echo $PYTHONPATH
+which python
+which python3.9
+
+
+if [ "${11}" -gt 0 ]
+  then
+  echo "Use FFCV"
+#sbatch --nodes=1 --time=07:00:00 --partition=small --gres=gpu:1 --mail-type=ALL --mail-user=sclaam@leeds.ac.uk  --error="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_confidence_ffcv.err" --output="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_confidence_ffcv.out" --job-name="${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_confidence_ffcv" slurm_confidence_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}" DATA_FOLDER="${data_folder}" OUTPUT_DIR="${output_dir}" TOPK=10
+
+python3.9 train_CIFAR10.py --ffcv --lr "0.1" --batch_size "${15}"  --save_folder "${14}" --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --epochs $6  --name $7 --width $8 --record $9 --input_resolution "${10}" --ffcv_train "${12}" --ffcv_val "${13}" --record_saturation 0 --resize 1
+
+#python3.9 ffcv_loaders.py
+
+# slurm_pruning_run.sh FFCV="${ffcv}" NAME="${name}" MODEL="${model}" DATASET="${dataset}"  NUMW=4  RFL="${rf_levels[$idxB]}" TYPE="normal" FOLDER="${directory}" PR="${pruning_rates[$idxA]}" EXPERIMENT=1 FFCV_TRAIN="${ffcv_train}" FFCV_VAL="${ffcv_val}"
+
+#./slurm_pruning_run.sh FFCV=0 NAME=recording_200_no_ffcv MODEL=resnet_small DATASET=small_imagenet NUMW=4 RFL=10 TYPE=normal FOLDER=$HOME/checkpoints PR=0.6 EXPERIMENT=1
+else
+  echo "Don't use FFCV"
+
+python3.9 train_CIFAR10.py --lr "0.1" --batch_size "${15}" --save_folder "${14}" --model $1 --dataset $2 --num_workers $3 --RF_level $4 --type $5 --epochs $6  --name $7 --width $8 --record $9 --input_resolution "${10}" --record_saturation 0 --resize 1
+
+#python3.9 ffcv_loaders.py
+
+  fi
+#echo "${model}_${rf_levels[$idxB]}_${dataset}_${pruning_rates[$idxA]}_pruning in directory ${directory}"
