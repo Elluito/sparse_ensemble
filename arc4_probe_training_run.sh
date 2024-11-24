6212e307e290fb366d52dbf466f5ce3a3a8ae2f2
Elluito <alfredoavendano@hotmail.com>
2024-11-24 16:12:05 +0000

    Second order optimisers

--- arc4_probe_training_run.sh
+++ arc4_probe_training_run.sh
@@ -4,7 +4,7 @@
 #$ -pe smp 16
 # Send emails when job starts and ends
 #$ -m be
-
+#$ -l h_vmem=32G
 
 #python3.9 -c "import os; print(os.environ)"
 ##python -c "import torch;device = 'cuda' if torch.cuda.is_available() else 'cpu';print(device);print('Cuda version with torch: {}'.format(torch.version.cuda))"
@@ -26,4 +26,4 @@
 
 unset GOMP_CPU_AFFINITY KMP_AFFINITY
 
-python train_probes.py -m $1 -d $2 --RF_level $3 --input_resolution $4 -mp 16 --save_path $5 --name $6
+python train_probes.py -m $1 -d $2 --RF_level $3 --input_resolution $4 -mp 0 --save_path $5 --name $6
