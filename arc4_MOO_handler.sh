a29ce962da2051f4863ddac0ab250d955abe0ea8
Elluito <alfredoavendano@hotmail.com>
2025-01-22 19:17:54 +0000

    table 3 aditional experiments

--- arc4_MOO_handler.sh
+++ arc4_MOO_handler.sh
@@ -2,14 +2,14 @@
 
 #o=$a({resnet18,resnet50,vgg19}" "{cifar10,cifar100}" "{tpe,nsga})
 for string in {resnet18,resnet50,vgg19}" "{cifar10,cifar100}" "{tpe,nsga};do
-  echo "${string}" "${string#*\ }"
+  echo "${string}"
 done
 
 for model in "resnet18" "resnet50" "vgg19";do
   for dataset in "cifar10" "cifar100";do
 
 
-      qsub -l h_rt=48:00:00 -l coproc_v100=1 arc4_MOO_RUN.sh "${model}" "${dataset}" "global" "nsga"
+      qsub -l h_rt=48:00:00 -l coproc_v100=1 -N "PARETO_${model}_${dataset}" arc4_MOO_RUN.sh "${model}" "${dataset}" "global" "nsga"
 
   done
 done
