33c64810ab0ad4dd33def00bd0caa377208c00fb
Elluito <alfredoavendano@hotmail.com>
2025-01-22 11:45:02 +0000

    table 3 aditional experiments

--- arc4_SP_fine_tuning_run.sh
+++ arc4_SP_fine_tuning_run.sh
@@ -20,8 +20,8 @@
 #python3.9 -c "import os; print(os.environ)"
 ##python -c "import torch;device = 'cuda' if torch.cuda.is_available() else 'cpu';print(device);print('Cuda version with torch: {}'.format(torch.version.cuda))"
 #
-#export LD_LIBRARY_PATH=""
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib"
+export LD_LIBRARY_PATH=""
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/nobackup/sclaam/.conda/envs/work/lib"
 #export PYTHONPATH="/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib/python3.9/site-packages"
 
 unset GOMP_CPU_AFFINITY KMP_AFFINITY
