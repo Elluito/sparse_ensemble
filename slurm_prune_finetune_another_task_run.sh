775846d59b1b6cb7c14e28333afccb5e5c7d9770
Elluito <alfredoavendano@hotmail.com>
2025-03-02 19:37:52 +0000

    I erased the other files accidentally

--- slurm_prune_finetune_another_task_run.sh
+++ slurm_prune_finetune_another_task_run.sh
@@ -127,6 +127,9 @@ echo "EXTRA_VALUES = $EXTRA_VALUES"
 #export LD_LIBRARY_PATH=""
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib"
 #export PYTHONPATH="/jmain02/home/J2AD014/mtc03/lla98-mtc03/.conda/envs/ffcv/lib/python3.9/site-packages"
+export LD_LIBRARY_PATH=""
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/users/sclaam/.conda/envs/work/lib"
+export PYTHONPATH="/users/sclaam/.conda/envs/work/lib/python3.9/site-packages"
 #############################################################
 #     One shot with specific pruning rate results
 #############################################################
