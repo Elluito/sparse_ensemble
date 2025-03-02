60f9ebc9772e5fc36c609d9f4393743bfbd7cb48
Elluito <alfredoavendano@hotmail.com>
2025-03-02 19:01:46 +0000

    I erased the other files accidentally

--- prune_models.py
+++ prune_models.py
@@ -3408,9 +3408,6 @@ if __name__ == '__main__':
                         help='Location to save the models', required=True)
     parser.add_argument('--resize', default=0, type=int,
                         help='Either resize the image to 32x32 and then back to input resolution')
-
-    parser.add_argument('--resize', default=0, type=int,
-                        help='Either resize the image to 32x32 and then back to input resolution')
     parser.add_argument('--name', default="", type=str, help='Name of the file', required=False)
     parser.add_argument('--solution', default="", type=str, help='Solution to use')
     parser.add_argument('--pruning_rate', default=0.9, type=float, help='Pruning rate')
