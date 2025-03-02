1b56521f19fdc88fe400518311d47b423a50f134
Elluito <alfredoavendano@hotmail.com>
2025-03-02 20:18:55 +0000

    I erased the other files accidentally

--- slurm_new_models_original_paper_handler.sh
+++ slurm_new_models_original_paper_handler.sh
@@ -12,7 +12,7 @@ for lvl in 0; do                # iterate idxa from 0 to length
 for model in  "resnet50";do
 for dataset in "cifar10"; do
 
-sbatch --nodes=1 --time=48:00:00 -t 1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_200_no_ffcv" 1 1
+sbatch --nodes=1 --time=47:00:00 -t 1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_200_no_ffcv" 1 1
 
 done
 done
@@ -21,7 +21,7 @@ done
 for lvl in 0; do    # iterate idxa from 0 to length
 for model in  "vgg19";do
 for dataset in "cifar10" "tiny_imagenet"; do
-sbatch --nodes=1 --time=48:00:00 -t 1-5--partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_200_no_ffcv" 1 1
+sbatch --nodes=1 --time=47:00:00 -t 1-5--partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_lvl_${lvl}_${dataset}" slurm_new_models_original_paper_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_200_no_ffcv" 1 1
 done
 done
 done
