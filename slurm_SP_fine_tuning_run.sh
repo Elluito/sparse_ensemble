3cd2d5927f1f9f5e09be8cb3c52cacb539c13903
Luis Alfredo <alfredoavendano@hotmail.com>
2025-04-26 23:05:56 +0100

    Grasp and Synflow for the experiments of figurse 8,9,10 and table 1

--- slurm_SP_fine_tuning_run.sh
+++ slurm_SP_fine_tuning_run.sh
@@ -23,7 +23,7 @@
 
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem-per-cpu=4GB
+#SBATCH --mem-per-cpu=8GB
 
 # send mail to this address
 #SBATCH --mail-user=sclaam@leeds.ac.uk
@@ -51,6 +51,6 @@ export LD_LIBRARY_PATH=""
 
 unset GOMP_CPU_AFFINITY KMP_AFFINITY
 
-python main.py -exp $1 -bs 32 --sigma $2 --pruner $3 --architecture $4 --dataset $5 --pruning_rate $6 --modeltype $7 --epochs $8 --name $9 -pop 1
+python main.py -exp $1 -bs 8 --sigma $2 --pruner $3 --architecture $4 --dataset $5 --pruning_rate $6 --modeltype $7 --epochs $8 --name $9 -pop 1
 
 #python main.py
