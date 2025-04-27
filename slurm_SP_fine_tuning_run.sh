ac4eacbf4107693d739954ec20999cb6031bb4ff
Luis Alfredo <alfredoavendano@hotmail.com>
2025-04-27 22:59:49 +0100

    Test to see if "general memory" can help with the OOM error and actually run.

--- slurm_SP_fine_tuning_run.sh
+++ slurm_SP_fine_tuning_run.sh
@@ -23,8 +23,8 @@
 
 # ####SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem-per-cpu=128G
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
 
 # send mail to this address
 #SBATCH --mail-user=sclaam@leeds.ac.uk
@@ -53,7 +53,7 @@ export PYTHONPATH="/users/sclaam/.conda/envs/work/lib/python3.9/site-packages"
 #unset GOMP_CPU_AFFINITY KMP_AFFINITY
 
 
-python main.py -exp $1 -bs 1 --sigma $2 --pruner $3 --architecture $4 --dataset $5 --pruning_rate $6 --modeltype $7 --epochs $8 --name $9 -pop 1 --num_workers 0
+python main.py -exp $1 -bs 1 --sigma $2 --pruner $3 --architecture $4 --dataset $5 --pruning_rate $6 --modeltype $7 --epochs $8 --name $9 -pop 1 --num_workers 8
 
 #python main.py
 
