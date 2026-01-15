19a06ddc0f89c88cb5de198006f6e672987af013
Luis Alfredo <alfredoavendano@hotmail.com>
2026-01-15 12:32:23 -0500

    Update

--- slurm_width_experiments_handler.sh
+++ slurm_width_experiments_handler.sh
@@ -11,6 +11,7 @@ for dataset in "cifar10"; do
 
 sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="width_${width}_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="width_${width}_${model}_lvl_${lvl}_${dataset}.out"  --job-name="width_${width}_${model}_lvl_${lvl}_${dataset}" slurm_width_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_width_${width}_200_no_ffcv" ${width} 1 ${resolution}
 
+
 done
 done
 done
