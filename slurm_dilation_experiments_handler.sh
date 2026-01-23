acd1ca4f14e9b4819152fa57e531f8187b3ca430
Luis Alfredo <alfredoavendano@hotmail.com>
2026-01-22 23:45:06 -0500

    solved "unexpected end of file" error

--- slurm_dilation_experiments_handler.sh
+++ slurm_dilation_experiments_handler.sh
@@ -26,7 +26,7 @@ for dataset in "cifar10"; do
 
 #sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}}_dilation_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
 
-sbatch --nodes=1 --time=47:00:00 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_${model}}_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
+sbatch --nodes=1 --time=47:00:00 --array=1-4 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_${model}}_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
 done
 done
 done
@@ -38,9 +38,37 @@ for dataset in "tiny_imagenet"; do
 
 #sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_dilation_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
 
-sbatch --nodes=1 --time=47:00:00 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_${model}_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
+sbatch --nodes=1 --time=47:00:00 --array=1-4 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_${model}_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
 
 done
 done
 done
 
+
+# RESNET50
+
+resolution=32
+for lvl in 54 407 159; do    # iterate idxa from 0 to length
+for model in  "resnet50_dilation";do
+for dataset in "cifar10"; do
+
+
+#sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}}_dilation_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
+
+sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_${model}}_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
+done
+done
+done
+
+resolution=64
+for lvl in 54 107 159; do    # iterate idxa from 0 to length
+for model in  "resnet50_dilation";do
+for dataset in "tiny_imagenet"; do
+
+#sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="${model}_lvl_${lvl}_${dataset}.out"  --job-name="${model}_dilation_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
+
+sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_${model}_lvl_${lvl}_${dataset}" slurm_dilation_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 200 "recording_dilation_200_no_ffcv" 1 1 ${resolution}
+
+done
+done
+done
