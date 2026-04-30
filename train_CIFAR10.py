6b223f2df94631288eb118bcf1b1fdd3b932850b
Luis Alfredo <alfredoavendano@hotmail.com>
2026-04-30 05:07:49 -0400

    FPGM pruning

--- train_CIFAR10.py
+++ train_CIFAR10.py
@@ -1330,7 +1330,7 @@ def main(args):
     fpgm_prune = args.fpgm_prune
     norm_rate = args.fpgm_norm_rate
     dist_rate = args.fpgm_dist_rate
-    pruning_interval = args.pruning_interval
+    pruning_interval = args.prune_interval
     if args.resume:
         scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args.epochs)
         # Load checkpoint.
