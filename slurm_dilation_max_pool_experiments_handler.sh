e01af7781598d6258d8ea3e7892d61e9e71634cc
Luis Alfredo <alfredoavendano@hotmail.com>
2026-07-03 23:11:27 +0100

    dilation + maxpool experiments

--- slurm_dilation_max_pool_experiments_handler.sh
+++ slurm_dilation_max_pool_experiments_handler.sh
@@ -7,7 +7,7 @@ for lvl in 1  91 180 269; do    # iterate idxa from 0 to length
 for model in  "vgg19_dilation_max_pool";do
 for dataset in "cifar10"; do
 
-sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
+sbatch --nodes=1 --time=47:00:00 --array=1-3 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
 done
 done
 done
@@ -20,7 +20,7 @@ for lvl in 1 54 107 159 407 655 907 1497 ; do    # iterate idxa from 0 to length
 for model in  "resnet50_dilation_max_pool";do
 for dataset in "cifar10"; do
 
-sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
+sbatch --nodes=1 --time=47:00:00 --array=1-3 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
 done
 done
 done
@@ -32,7 +32,7 @@ for lvl in 1 54 107 159 407 655 907 1497; do    # iterate idxa from 0 to length
 for model in  "resnet50_dilation_max_pool";do
 for dataset in "tiny_imagenet"; do
 
-sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
+sbatch --nodes=1 --time=47:00:00 --array=1-3 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
 
 done
 done
@@ -45,7 +45,7 @@ for lvl in 1 91 180 269 ; do    # iterate idxa from 0 to length
 for model in  "vgg19_dilation_max_pool";do
 for dataset in "tiny_imagenet"; do
 
-sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
+sbatch --nodes=1 --time=47:00:00 --array=1-3 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
 
 done
 done
