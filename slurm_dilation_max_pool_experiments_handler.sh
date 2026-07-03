c03deb61b26bf743fd32d27ea4cf47abd50a7bb1
Luis Alfredo <alfredoavendano@hotmail.com>
2026-07-03 23:10:55 +0100

    dilation + maxpool experiments

--- slurm_dilation_max_pool_experiments_handler.sh
+++ slurm_dilation_max_pool_experiments_handler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
-# VGG19 dilation + maxpool
+# CIFAR10
+## VGG19 dilation + maxpool
 
 resolution=32
 for lvl in 1  91 180 269; do    # iterate idxa from 0 to length
@@ -12,19 +12,8 @@ done
 done
 done
 
-resolution=64
-for lvl in 1 91 180 269 ; do    # iterate idxa from 0 to length
-for model in  "vgg19_dilation_max_pool";do
-for dataset in "tiny_imagenet"; do
 
-sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
-
-done
-done
-done
-
-
-# RESNET50 dilation + maxpool
+## RESNET50 dilation + maxpool
 
 resolution=32
 for lvl in 1 54 107 159 407 655 907 1497 ; do    # iterate idxa from 0 to length
@@ -36,9 +25,24 @@ done
 done
 done
 
+# CIFAR10
+## RESNET50 dilation + maxpool
 resolution=64
 for lvl in 1 54 107 159 407 655 907 1497; do    # iterate idxa from 0 to length
 for model in  "resnet50_dilation_max_pool";do
+for dataset in "tiny_imagenet"; do
+
+sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
+
+done
+done
+done
+
+## VGG19 dilation + maxpool
+
+resolution=64
+for lvl in 1 91 180 269 ; do    # iterate idxa from 0 to length
+for model in  "vgg19_dilation_max_pool";do
 for dataset in "tiny_imagenet"; do
 
 sbatch --nodes=1 --time=47:00:00 --array=1-5 --partition=gpu  --mail-type=all --mail-user=sclaam@leeds.ac.uk --error="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.err" --gres=gpu:1 --output="dilation_max_pool_${model}_lvl_${lvl}_${dataset}.out"  --job-name="dilation_max_pool_${model}_lvl_${lvl}_${dataset}" slurm_dilation_max_pool_experiments_run.sh "${model}" "${dataset}" 8 ${lvl}  "normal" 100 "recording_dilation_max_pool_100_no_ffcv" 1 1 ${resolution}
